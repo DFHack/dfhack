1ac83c8a0fabd31a34aed5bd74882db77cf047a2
Petr Mrázek <peterix@gmail.com>
2009-10-23 14:21:56 +0000

    fix mingw build

--- library/integers.h
+++ library/integers.h
@@ -6,7 +6,7 @@ Visual Studio doesn't have stdint.h
 stdint.h is part of the C99 standard. It's ancient and simply should be there. FAIL
 */
 
-#ifdef LINUX_BUILD
+#ifndef _MSC_VER
 #include <stdint.h>
 #else
 #include <stdint_win.h>

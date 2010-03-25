da6874d7250110a61eb426e6f986be2547e1fbd4
NMLittle <nmlittle@gmail.com>
2010-03-25 16:59:46 -0500

    another temp code for review

--- tools/magma_create.cpp
+++ tools/magma_create.cpp
@@ -30,10 +30,11 @@ int main (void)
             if(DF.isValidBlock(x,y,z))
             {
                 DF.ReadDesignations((x/16),(y/16),(z/16), &designations);
-
+                cout << x%16;
                 designations[x%16][y%16].bits.flow_size = 7;
                 designations[x%16][y%16].bits.liquid_type = DFHack::liquid_magma;
                 DF.WriteDesignations(x,y,z, &designations);
+                cout << "Success" << endl;
             }
         }
     }

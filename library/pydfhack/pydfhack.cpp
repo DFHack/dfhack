478974e399a7c5359c828f3df0bbd882f2d7f9a9
doomchild <doomchild@theyfear.us>
2010-04-03 21:01:11 -0500

    removed UnionBase.cpp dependency

--- library/pydfhack/pydfhack.cpp
+++ library/pydfhack/pydfhack.cpp
@@ -23,7 +23,7 @@ distribution.
 */
 
 #include "Python.h"
-#include "UnionBase.cpp"
+//#include "UnionBase.cpp"
 #include "DF_API.cpp"
 
 #ifndef PyMODINIT_FUNC
@@ -39,17 +39,17 @@ PyMODINIT_FUNC initpydfhack(void)
 {
 	PyObject* module;
 	
-	if(PyType_Ready(&UnionBase_type) < 0)
-		return;
+	// if(PyType_Ready(&UnionBase_type) < 0)
+		// return;
 	
 	if(PyType_Ready(&DF_API_type) < 0)
 		return;
 	
 	module = Py_InitModule3("pydfhack", module_methods, "pydfhack extension module");
 	
-	Py_INCREF(&UnionBase_type);
+	//Py_INCREF(&UnionBase_type);
 	Py_INCREF(&DF_API_type);
 	
-	PyModule_AddObject(module, "UnionBase", (PyObject*)&UnionBase_type);
+	//PyModule_AddObject(module, "UnionBase", (PyObject*)&UnionBase_type);
 	PyModule_AddObject(module, "API", (PyObject*)&DF_API_type);
 }

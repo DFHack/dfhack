c7217cbdf798503906ae4b08f9e03a1944d72cf4
doomchild <lee.crabtree@gmail.com>
2010-04-05 11:08:55 -0500

    modified setup script to export ReadRaw and WriteRaw

--- dfhack/python/setup.py
+++ dfhack/python/setup.py
@@ -2,9 +2,10 @@
 from distutils.core import setup, Extension
 
 e = Extension("pydfhack", 
-		sources=["UnionBase.cpp", "pydfhack.cpp", "DF_API.cpp"],
+		sources=["pydfhack.cpp", "DF_API.cpp"],
         include_dirs=["..\\include"],
 		library_dirs=["..\\..\\output"],
-		libraries=["libdfhack"])
+		libraries=["libdfhack"],
+        export_symbols=["initpydfhack", "ReadRaw", "WriteRaw"])
 
 setup(name="PyDFHack", version="1.0", ext_modules=[e])

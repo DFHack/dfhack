d5aa1488a600389b48c6b703893cb5f44536b2cc
Petr Mrázek <peterix@gmail.com>
2010-04-06 13:05:54 +0200

    Magma create works

--- examples/veccheck.cpp
+++ examples/veccheck.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <sstream>
 #include <ctime>
+#include <cstdio>
 using namespace std;
 
 #include <DFTypes.h>
@@ -13,6 +14,7 @@ using namespace std;
 #include <DFProcess.h>
 #include <DFMemInfo.h>
 #include <DFVector.h>
+#include <DFTypes.h>
 
 void DumpObjStr0Vector (const char * name, DFHack::Process *p, uint32_t addr)
 {
@@ -25,7 +27,18 @@ void DumpObjStr0Vector (const char * name, DFHack::Process *p, uint32_t addr)
     }
     cout << endl;
 }
-
+void DumpObjVtables (const char * name, DFHack::Process *p, uint32_t addr)
+{
+    cout << "----==== " << name << " ====----" << endl;
+    DFHack::DfVector vect(p,addr,4);
+    for(int i = 0; i < vect.getSize();i++)
+    {
+        uint32_t addr = *(uint32_t *) vect[i];
+        uint32_t vptr = p->readDWord(addr);
+        cout << p->readClassName(vptr) << endl;
+    }
+    cout << endl;
+}
 void DumpDWordVector (const char * name, DFHack::Process *p, uint32_t addr)
 {
     cout << "----==== " << name << " ====----" << endl;
@@ -63,11 +76,41 @@ int main (int numargs, const char ** args)
     DFHack::Process* p = DF.getProcess();
     DFHack::memory_info* mem = DF.getMemoryInfo();
     //const vector<string> * names = mem->getClassIDMapping();
+/*    
+    string name="Stuff";
+    cout << "----==== " << name << " ====----" << endl;
+    DFHack::DfVector vect(p,0x165b290,4);
+    for(int i = 0; i < vect.getSize();i++)
+    {
+        uint32_t addr = *(uint32_t *) vect[i];
+        DFHack::t_construction_df40d constr;
+        DF.ReadRaw(addr, sizeof(constr), (uint8_t *) &constr);
+        printf("0x%x %dX %dY %dZ: %d:%d\n", addr, constr.x, constr.y, constr.z,
+               constr.material.type,constr.material.index);
+    }
     
+    
+    cout << endl;
+  */  
+    /*
+    DumpObjVtables("Constructions?",p,0x165b278);
+    DumpObjVtables("Constructions?",p,0x166edb8);
+    */
+    /*
     DumpObjStr0Vector("Material templates",p, mem->getAddress("mat_templates"));
-    
+    */
     DumpObjStr0Vector("Inorganics",p, mem->getAddress("mat_inorganics"));
     
+    cout << "----==== Inorganics ====----" << endl;
+    DFHack::DfVector vect(p,addr,4);
+    for(int i = 0; i < vect.getSize();i++)
+    {
+        uint32_t addr = *(uint32_t *) vect[i];
+        cout << p->readSTLString(addr) << endl;
+    }
+    cout << endl;
+    
+    /*
     DumpObjStr0Vector("Organics - all",p, mem->getAddress("mat_organics_all"));
     
     DumpObjStr0Vector("Organics - plants",p, mem->getAddress("mat_organics_plants"));
@@ -89,7 +132,7 @@ int main (int numargs, const char ** args)
     DumpObjStr0Vector("Creature variations",p, mem->getAddress("mat_creature_variations"));
     
     DumpObjStr0Vector("Creature types",p, mem->getAddress("mat_creature_types"));
-    
+    */
     
     #ifndef LINUX_BUILD
     cout << "Done. Press any key to continue" << endl;

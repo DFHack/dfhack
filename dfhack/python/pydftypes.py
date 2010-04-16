9c6d889a2467996a593a92f8879eeed465fa1f4a
doomchild <lee.crabtree@gmail.com>
2010-04-16 11:59:38 -0500

    added vein types

--- dfhack/python/pydftypes.py
+++ dfhack/python/pydftypes.py
@@ -2,6 +2,9 @@ from collections import namedtuple
 
 Note = namedtuple("Note", "symbol, foreground, background, name, position")
 Construction = namedtuple("Construction", "position, form, unk_8, mat_type, mat_idx, unk3, unk4, unk5, unk6, origin")
+Vein = namedtuple("Vein", "vtable, type, flags, address, assignment")
+FrozenLiquidVein = namedtuple("FrozenLiquidVein", "vtable, address, tiles")
+SpatterVein = namedtuple("SpatterVein", "vtable, address, mat1, unk1, mat2, mat3, intensity")
 
 class Name(object):
     __slots__ = ["first_name", "nickname", "language", "has_name", "words", "parts_of_speech"]

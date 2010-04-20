2b544ba2de9ae6d201171fb4d7ffecea9a387995
doomchild <lee.crabtree@gmail.com>
2010-04-20 09:19:23 -0500

    updated compiler flags to get rid of stupid newline warnings

--- dfhack/python/setup.py
+++ dfhack/python/setup.py
@@ -2,9 +2,10 @@
 from distutils.core import setup, Extension
 
 e = Extension("pydfhack", 
-		sources=["DF_MemInfo.cpp", "DF_API.cpp", "pydfhack.cpp"],
+		sources=["pydfhack.cpp"],
         include_dirs=["..\\", "..\\include", "..\\depends\\md5", "..\\depends\\tinyxml"],
 		library_dirs=["..\\..\\output"],
+        extra_compile_args=["-w"],
 		libraries=["libdfhack"],
         export_symbols=["initpydfhack", "ReadRaw", "WriteRaw"])
 

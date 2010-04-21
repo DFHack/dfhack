662bb2291b5d4b0f86f9ff35687e29cbb1505344
doomchild <lee.crabtree@gmail.com>
2010-04-21 14:58:17 -0500

    added base type assignments

--- dfhack/python/pydfapi.py
+++ dfhack/python/pydfapi.py
@@ -3,6 +3,10 @@ from pydfhack import *
 class API(_API):
     def __init__(self, *args, **kwds):
         _API.__init__(self, args, kwds)
+        
+        self._map_mgr_type = Map
+        self._vegetation_mgr_type = Vegetation
+        self._gui_mgr_type = GUI
 
 class Map(_MapManager):
     def __init__(self, *args, **kwds):

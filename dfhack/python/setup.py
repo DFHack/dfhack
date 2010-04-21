d148aeecc893561b7be126cb3d82b4325d5b0bed
doomchild <lee.crabtree@gmail.com>
2010-04-21 14:57:53 -0500

    updated build script

--- dfhack/python/setup.py
+++ dfhack/python/setup.py
@@ -2,10 +2,10 @@
 from distutils.core import setup, Extension
 
 e = Extension("pydfhack", 
-		sources=["pydfhack.cpp"],
+		sources=["DF_API.cpp", "DF_Buildings.cpp", "DF_Constructions.cpp", "DF_CreatureManager.cpp", "DF_GUI.cpp", "DF_Maps.cpp", "DF_Material.cpp", "DF_Position.cpp", "DF_Translate.cpp", "DF_Vegetation.cpp", "pydfhack.cpp"],
         include_dirs=["..\\", "..\\include", "..\\depends\\md5", "..\\depends\\tinyxml"],
 		library_dirs=["..\\..\\output"],
-        extra_compile_args=["-w"],
+        #extra_compile_args=["-w"],
 		libraries=["libdfhack"],
         export_symbols=["initpydfhack", "ReadRaw", "WriteRaw"])
 

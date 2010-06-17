5b71ac7dae0c622a843a116cd58590dccf9d2d17
doomchild <lee.crabtree@gmail.com>
2010-06-17 16:26:53 -0500

    add __init__ file renamed flags and types

---
+++ library/python/pydfhack/__init__.py
@@ -0,0 +1,14 @@
+import context
+
+__all__ = [ "buildings", 
+                "constructions", 
+                "context", 
+                "creatures", 
+                "dftypes",
+                "flags",
+                "gui", 
+                "items", 
+                "maps", 
+                "materials",
+                "position"
+                "vegetation" ]

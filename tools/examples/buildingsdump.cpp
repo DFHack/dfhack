efe6854e3d784fda189c3b9793f59115a342e3b0
Petr Mrázek <peterix@gmail.com>
2010-07-27 04:32:51 +0200

    furnaces and smelters ... and stuff :)

--- tools/examples/buildingsdump.cpp
+++ tools/examples/buildingsdump.cpp
@@ -79,8 +79,9 @@ int main (int argc,const char* argv[])
                     {
                         //cout << buildingtypes[temp.type] << " 0x" << hex << temp.origin << endl;
                         //hexdump(DF, temp.origin, 16);
-                        addresses.push_back(temp.origin);
+                        //addresses.push_back(temp.origin);
                     }
+                                        addresses.push_back(temp.origin);
                 }
                 else
                 {

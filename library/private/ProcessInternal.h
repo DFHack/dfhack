9964dbac72539b3cc1dbd32ad623b4d10c5d440a
Tom Prince <tom.prince@ualberta.net>
2011-02-23 01:07:22 -0500

    Add missing private/ProcessInternal.h

---
+++ library/private/ProcessInternal.h
@@ -0,0 +1,231 @@
+/*
+www.sourceforge.net/projects/dfhack
+Copyright (c) 2009 Petr Mrázek (peterix), Kenneth Ferland (Impaler[WrG]), dorf
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any
+damages arising from the use of this software.
+
+Permission is granted to anyone to use this software for any
+purpose, including commercial applications, and to alter it and
+redistribute it freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must
+not claim that you wrote the original software. If you use this
+software in a product, an acknowledgment in the product documentation
+would be appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and
+must not be misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source
+distribution.
+*/
+
+#ifndef PROCESS_INTERNAL_H_INCLUDED
+#define PROCESS_INTERNAL_H_INCLUDED
+
+#include "dfhack/DFProcess.h"
+
+namespace DFHack
+{
+    ////////////////////////////////////////////////////////////////////////////
+    //         Compiler appeasement area. Not worth a look really...          //
+    ////////////////////////////////////////////////////////////////////////////
+
+    class DFHACK_EXPORT NormalProcess : public Process
+    {
+        friend class ProcessEnumerator;
+        class Private;
+        private:
+            Private * const d;
+        public:
+            NormalProcess(uint32_t pid, std::vector <VersionInfo *> & known_versions);
+            ~NormalProcess();
+            bool attach();
+            bool detach();
+
+            bool suspend();
+            bool asyncSuspend();
+            bool resume();
+            bool forceresume();
+
+            uint64_t readQuad(const uint32_t address);
+            void readQuad(const uint32_t address, uint64_t & value);
+            void writeQuad(const uint32_t address, const uint64_t value);
+
+            uint32_t readDWord(const uint32_t address);
+            void readDWord(const uint32_t address, uint32_t & value);
+            void writeDWord(const uint32_t address, const uint32_t value);
+
+            float readFloat(const uint32_t address);
+            void readFloat(const uint32_t address, float & value);
+
+            uint16_t readWord(const uint32_t address);
+            void readWord(const uint32_t address, uint16_t & value);
+            void writeWord(const uint32_t address, const uint16_t value);
+
+            uint8_t readByte(const uint32_t address);
+            void readByte(const uint32_t address, uint8_t & value);
+            void writeByte(const uint32_t address, const uint8_t value);
+
+            void read( uint32_t address, uint32_t length, uint8_t* buffer);
+            void write(uint32_t address, uint32_t length, uint8_t* buffer);
+
+            const std::string readSTLString (uint32_t offset);
+            size_t readSTLString (uint32_t offset, char * buffer, size_t bufcapacity);
+            void writeSTLString(const uint32_t address, const std::string writeString){};
+            // get class name of an object with rtti/type info
+            std::string readClassName(uint32_t vptr);
+
+            const std::string readCString (uint32_t offset);
+
+            bool isSuspended();
+            bool isAttached();
+            bool isIdentified();
+
+            bool getThreadIDs(std::vector<uint32_t> & threads );
+            void getMemRanges(std::vector<t_memrange> & ranges );
+            VersionInfo *getDescriptor();
+            int getPID();
+            std::string getPath();
+            // get module index by name and version. bool 1 = error
+            bool getModuleIndex (const char * name, const uint32_t version, uint32_t & OUTPUT) { OUTPUT=0; return false;};
+            // get the SHM start if available
+            char * getSHMStart (void){return 0;};
+            // set a SHM command and wait for a response
+            bool SetAndWait (uint32_t state){return false;};
+    };
+
+    class DFHACK_EXPORT SHMProcess : public Process
+    {
+        friend class ProcessEnumerator;
+        class Private;
+        private:
+            Private * const d;
+
+        public:
+            SHMProcess(uint32_t PID, std::vector <VersionInfo *> & known_versions);
+            ~SHMProcess();
+            // Set up stuff so we can read memory
+            bool attach();
+            bool detach();
+
+            bool suspend();
+            bool asyncSuspend();
+            bool resume();
+            bool forceresume();
+
+            uint64_t readQuad(const uint32_t address);
+            void readQuad(const uint32_t address, uint64_t & value);
+            void writeQuad(const uint32_t address, const uint64_t value);
+
+            uint32_t readDWord(const uint32_t address);
+            void readDWord(const uint32_t address, uint32_t & value);
+            void writeDWord(const uint32_t address, const uint32_t value);
+
+            float readFloat(const uint32_t address);
+            void readFloat(const uint32_t address, float & value);
+
+            uint16_t readWord(const uint32_t address);
+            void readWord(const uint32_t address, uint16_t & value);
+            void writeWord(const uint32_t address, const uint16_t value);
+
+            uint8_t readByte(const uint32_t address);
+            void readByte(const uint32_t address, uint8_t & value);
+            void writeByte(const uint32_t address, const uint8_t value);
+
+            void read( uint32_t address, uint32_t length, uint8_t* buffer);
+            void write(uint32_t address, uint32_t length, uint8_t* buffer);
+
+            const std::string readSTLString (uint32_t offset);
+            size_t readSTLString (uint32_t offset, char * buffer, size_t bufcapacity);
+            void writeSTLString(const uint32_t address, const std::string writeString);
+            // get class name of an object with rtti/type info
+            std::string readClassName(uint32_t vptr);
+
+            const std::string readCString (uint32_t offset);
+
+            bool isSuspended();
+            bool isAttached();
+            bool isIdentified();
+
+            bool getThreadIDs(std::vector<uint32_t> & threads );
+            void getMemRanges(std::vector<t_memrange> & ranges );
+            VersionInfo *getDescriptor();
+            int getPID();
+            std::string getPath();
+            // get module index by name and version. bool 1 = error
+            bool getModuleIndex (const char * name, const uint32_t version, uint32_t & OUTPUT);
+            // get the SHM start if available
+            char * getSHMStart (void);
+            bool SetAndWait (uint32_t state);
+    };
+
+#ifdef LINUX_BUILD
+    class DFHACK_EXPORT WineProcess : public Process
+    {
+        friend class ProcessEnumerator;
+        class Private;
+        private:
+            Private * const d;
+
+        public:
+            WineProcess(uint32_t pid, std::vector <VersionInfo *> & known_versions);
+            ~WineProcess();
+            bool attach();
+            bool detach();
+
+            bool suspend();
+            bool asyncSuspend();
+            bool resume();
+            bool forceresume();
+
+            uint64_t readQuad(const uint32_t address);
+            void readQuad(const uint32_t address, uint64_t & value);
+            void writeQuad(const uint32_t address, const uint64_t value);
+
+            uint32_t readDWord(const uint32_t address);
+            void readDWord(const uint32_t address, uint32_t & value);
+            void writeDWord(const uint32_t address, const uint32_t value);
+
+            float readFloat(const uint32_t address);
+            void readFloat(const uint32_t address, float & value);
+
+            uint16_t readWord(const uint32_t address);
+            void readWord(const uint32_t address, uint16_t & value);
+            void writeWord(const uint32_t address, const uint16_t value);
+
+            uint8_t readByte(const uint32_t address);
+            void readByte(const uint32_t address, uint8_t & value);
+            void writeByte(const uint32_t address, const uint8_t value);
+
+            void read( uint32_t address, uint32_t length, uint8_t* buffer);
+            void write(uint32_t address, uint32_t length, uint8_t* buffer);
+
+            const std::string readSTLString (uint32_t offset);
+            size_t readSTLString (uint32_t offset, char * buffer, size_t bufcapacity);
+            void writeSTLString(const uint32_t address, const std::string writeString){};
+            // get class name of an object with rtti/type info
+            std::string readClassName(uint32_t vptr);
+
+            const std::string readCString (uint32_t offset);
+
+            bool isSuspended();
+            bool isAttached();
+            bool isIdentified();
+
+            bool getThreadIDs(std::vector<uint32_t> & threads );
+            void getMemRanges(std::vector<t_memrange> & ranges );
+            VersionInfo *getDescriptor();
+            int getPID();
+            // get module index by name and version. bool 1 = error
+            bool getModuleIndex (const char * name, const uint32_t version, uint32_t & OUTPUT) {OUTPUT=0; return false;};
+            // get the SHM start if available
+            char * getSHMStart (void){return 0;};
+            bool SetAndWait (uint32_t state){return false;};
+            std::string getPath();
+    };
+#endif
+}
+#endif

1481b07b6bd23bf336d7cba8dfc6e95f73ed32b4
Petr Mrázek <peterix@gmail.com>
2011-03-19 23:26:32 +0100

    Ubuntu packaging magic, forgotten header files, an icon.

---
+++ library/private/ModuleFactory.h
@@ -0,0 +1,44 @@
+/*
+ w ww.so*urceforge.net/projects/dfhack
+ Copyright (c) 2009 Petr Mrázek (peterix), Kenneth Ferland (Impaler[WrG]), dorf
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty. In no event will the authors be held liable for any
+ damages arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any
+ purpose, including commercial applications, and to alter it and
+ redistribute it freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must
+ not claim that you wrote the original software. If you use this
+ software in a product, an acknowledgment in the product documentation
+ would be appreciated but is not required.
+ 
+ 2. Altered source versions must be plainly marked as such, and
+ must not be misrepresented as being the original software.
+ 
+ 3. This notice may not be removed or altered from any source
+ distribution.
+ */
+
+#ifndef MODULE_FACTORY_H_INCLUDED
+#define MODULE_FACTORY_H_INCLUDED
+
+namespace DFHack
+{
+    class Module;
+    class DFContextShared;
+    Module* createCreatures(DFContextShared * d);
+    Module* createGui(DFContextShared * d);
+    Module* createWindowIO(DFContextShared * d);
+    Module* createWorld(DFContextShared * d);
+    Module* createMaterials(DFContextShared * d);
+    Module* createItems(DFContextShared * d);
+    Module* createTranslation(DFContextShared * d);
+    Module* createVegetation(DFContextShared * d);
+    Module* createBuildings(DFContextShared * d);
+    Module* createConstructions(DFContextShared * d);
+    Module* createMaps(DFContextShared * d);
+}
+#endif

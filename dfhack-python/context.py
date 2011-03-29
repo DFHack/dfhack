ea54dc645cc0c921496a1fee9ccf7b7a5026ff81
doomchild <lee.crabtree@gmail.com>
2011-03-29 10:07:50 -0500

    removed references to position module, which no longer exists

--- dfhack-python/context.py
+++ dfhack-python/context.py
@@ -15,7 +15,6 @@ libdfhack.Context_getProcess.restype = c_void_p
 libdfhack.Context_getCreatures.restype = c_void_p
 libdfhack.Context_getMaps.restype = c_void_p
 libdfhack.Context_getGui.restype = c_void_p
-libdfhack.Context_getPosition.restype = c_void_p
 libdfhack.Context_getMaterials.restype = c_void_p
 libdfhack.Context_getTranslation.restype = c_void_p
 libdfhack.Context_getVegetation.restype = c_void_p
@@ -58,7 +57,6 @@ class Context(object):
     def __init__(self, ptr):
         self._c_ptr = ptr
 
-        self._pos_obj = None
         self._mat_obj = None
         self._map_obj = None
         self._veg_obj = None
@@ -99,14 +97,6 @@ class Context(object):
     @property
     def is_suspended(self):
         return libdfhack.Context_isSuspended(self._c_ptr) > 0
-
-    @property
-    def position(self):
-        import position
-        if self._pos_obj is None:
-            self._pos_obj = position.Position(libdfhack.Context_getPosition(self._c_ptr))
-
-        return self._pos_obj
 
     @property
     def materials(self):
@@ -187,3 +177,8 @@ class Context(object):
             self._window_io_obj = window_io.WindowIO(libdfhack.Context_getWindowIO(self._c_ptr))
         
         return self._window_io_obj
+
+cm = ContextManager("Memory.xml")
+df = cm.get_single_context()
+df.attach()
+w = df.window_io

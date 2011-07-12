b5a6087da599f85ef1ab663358aa06463948d653
Petr Mrázek <peterix@gmail.com>
2011-07-12 23:27:09 +0200

    Forgot to commit a file.

---
+++ library/Virtual.cpp
@@ -0,0 +1,16 @@
+#include "Internal.h"
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include "dfhack/Process.h"
+#include "dfhack/Core.h"
+#include "dfhack/Virtual.h"
+using namespace DFHack;
+
+std::string t_virtual::getClassName()
+{
+    Core & c = Core::getInstance();
+    return c.p->readClassName(vptr);
+}

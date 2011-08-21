246f704c680abe5fe32d039c5069b146c96db881
Warmist <warmist@gmail.com>
2011-08-21 15:56:42 +0300

    Move function found for linux.

--- plugins/Dfusion/luafiles/onfunction/plugin.lua
+++ plugins/Dfusion/luafiles/onfunction/plugin.lua
@@ -12,7 +12,9 @@ else
 		onfunction.AddFunction(0x55499D+offsets.base(),"Move") --on creature move found with "watch mem=xcoord"
 		onfunction.AddFunction(0x275933+offsets.base(),"Die")  --on creature death? found by watching dead flag then stepping until new function
 	else
-		
+		--onfunction.AddFunction(0x0899be82+offsets.base(),"Move") -- found out by attaching watch...
+		onfunction.AddFunction(0x899befe+offsets.base(),"Move") -- found out by attaching watch...
+
 	end
 	onfunction.SetCallback("Die",DeathMsg)
 end

81ad287c0621e3655782696b2ce1369ef6ae8e8e
Alexander Gavrilov <angavrilov@gmail.com>
2011-12-24 16:22:10 +0400

    Generate very simple static functions to find objects in global vectors.

--- library/DataStatics.cpp
+++ library/DataStatics.cpp
@@ -1,5 +1,17 @@
 #include "Internal.h"
 #include "dfhack/DataDefs.h"
+#include "dfhack/MiscUtils.h"
+
+#include "dfhack/df/world.h"
+#include "dfhack/df/world_data.h"
+#include "dfhack/df/ui.h"
+
+namespace {
+    template<class T>
+    inline T &_toref(T &r) { return r; }
+    template<class T>
+    inline T &_toref(T *&p) { return *p; }
+}
 
 // Instantiate all the static objects
 #include "dfhack/df/static.inc"

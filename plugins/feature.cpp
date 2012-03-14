a05cbcc2de1921ba02a937333246a8410eed8296
Alexander Gavrilov <angavrilov@gmail.com>
2012-03-14 10:56:30 +0400

    Rename the world substruct with the save directory name.

--- plugins/feature.cpp
+++ plugins/feature.cpp
@@ -33,9 +33,9 @@ static command_result feature(color_ostream &out, vector <string> &parameters)
     {
         if (parameters.size() != 1)
             return CR_WRONG_USAGE;
-        for (size_t i = 0; i < world->unk_192bd8.map_features.size(); i++)
+        for (size_t i = 0; i < world->cur_savegame.map_features.size(); i++)
         {
-            df::feature_init *feature_init = world->unk_192bd8.map_features[i];
+            df::feature_init *feature_init = world->cur_savegame.map_features[i];
             string name;
             feature_init->getName(&name);
             out.print("Feature #%i (\"%s\", type %s) is %s\n", i, name.c_str(), ENUM_KEY_STR(feature_type, feature_init->getType()), feature_init->flags.is_set(feature_init_flags::Discovered) ? "discovered" : "hidden");
@@ -46,12 +46,12 @@ static command_result feature(color_ostream &out, vector <string> &parameters)
         if (parameters.size() != 2)
             return CR_WRONG_USAGE;
         size_t i = atoi(parameters[1].c_str());
-        if ((i < 0) || (i >= world->unk_192bd8.map_features.size()))
+        if ((i < 0) || (i >= world->cur_savegame.map_features.size()))
         {
             out.print("No such feature!\n");
             return CR_FAILURE;
         }
-        df::feature_init *feature_init = world->unk_192bd8.map_features[i];
+        df::feature_init *feature_init = world->cur_savegame.map_features[i];
         if (feature_init->flags.is_set(feature_init_flags::Discovered))
         {
             out.print("Selected feature is already discovered!\n");
@@ -67,12 +67,12 @@ static command_result feature(color_ostream &out, vector <string> &parameters)
         if (parameters.size() != 2)
             return CR_WRONG_USAGE;
         size_t i = atoi(parameters[1].c_str());
-        if ((i < 0) || (i >= world->unk_192bd8.map_features.size()))
+        if ((i < 0) || (i >= world->cur_savegame.map_features.size()))
         {
             out.print("No such feature!\n");
             return CR_FAILURE;
         }
-        df::feature_init *feature_init = world->unk_192bd8.map_features[i];
+        df::feature_init *feature_init = world->cur_savegame.map_features[i];
         if (!feature_init->flags.is_set(feature_init_flags::Discovered))
         {
             out.print("Selected feature is already hidden!\n");

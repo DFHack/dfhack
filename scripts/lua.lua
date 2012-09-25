cc5df57e53bf89b8cce51353c0a8b4f048d23679
Warmist <Warmist@gmail.com>
2012-09-25 10:24:45 +0300

    Little error fixed in lua script

--- scripts/lua.lua
+++ scripts/lua.lua
@@ -3,7 +3,7 @@ if args[1]=="--file" or args[1]=="-f" then
     local f=loadfile (args[2])
     dfhack.pcall(f,table.unpack(args,3))
 elseif args[1]~=nil then
-    local f=load(args[1],'=(lua command)', 't',)
+    local f=load(args[1],'=(lua command)', 't')
     dfhack.pcall(f,table.unpack(args,2))
 else
     dfhack.interpreter("lua","lua.history")

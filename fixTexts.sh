bb39b2e45bdbe8fda2f99607e9ca56319a602843
Petr Mrázek <peterix@gmail.com>
2012-09-30 06:55:52 +0200

    Lua API, not LUA Api.

--- fixTexts.sh
+++ fixTexts.sh
@@ -2,5 +2,5 @@
 # this script is used for easy testing of the rst documentation files.
 rst2html  Readme.rst Readme.html
 rst2html  Compile.rst Compile.html
-rst2html  LUA\ Api.rst LUA\ Api.html
+rst2html  Lua\ API.rst Lua\ API.html
 rst2html  Contributors.rst > Contributors.html

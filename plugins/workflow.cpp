a7bf526f41216f6e43c942c21907dbd46fe8f44f
Alexander Gavrilov <angavrilov@gmail.com>
2012-11-08 21:27:56 +0400

    Make workflow consider squad-assigned items busy.

--- plugins/workflow.cpp
+++ plugins/workflow.cpp
@@ -1004,6 +1004,12 @@ static bool isRouteVehicle(df::item *item)
     return vehicle && vehicle->route_id >= 0;
 }
 
+static bool isAssignedSquad(df::item *item)
+{
+    auto &vec = ui->equipment.items_assigned[item->getType()];
+    return binsearch_index(vec, &df::item::id, item->id) >= 0;
+}
+
 static void map_job_items(color_ostream &out)
 {
     for (size_t i = 0; i < constraints.size(); i++)
@@ -1117,8 +1123,10 @@ static void map_job_items(color_ostream &out)
                 item->isAssignedToStockpile() ||
                 isRouteVehicle(item) ||
                 itemInRealJob(item) ||
-                itemBusy(item))
+                itemBusy(item) ||
+                isAssignedSquad(item))
             {
+                is_invalid = true;
                 cv->item_inuse++;
             }
             else

4dfe46e26f139167cc51ec689c19b3ba08747c0d
jj <john-git@ofjj.net>
2012-11-24 16:52:21 +0100

    manipulator: fix column width calculations for 80x25 window

--- plugins/manipulator.cpp
+++ plugins/manipulator.cpp
@@ -469,10 +469,10 @@ void viewscreen_unitlaborsst::calcSize()
     int col_maxwidth[DISP_COLUMN_MAX];
     col_minwidth[DISP_COLUMN_HAPPINESS] = 4;
     col_maxwidth[DISP_COLUMN_HAPPINESS] = 4;
-    col_minwidth[DISP_COLUMN_NAME] = 0;
-    col_maxwidth[DISP_COLUMN_NAME] = 0;
-    col_minwidth[DISP_COLUMN_PROFESSION] = 0;
-    col_maxwidth[DISP_COLUMN_PROFESSION] = 0;
+    col_minwidth[DISP_COLUMN_NAME] = 15;
+    col_maxwidth[DISP_COLUMN_NAME] = 15;        // adjusted in the loop below
+    col_minwidth[DISP_COLUMN_PROFESSION] = 15;
+    col_maxwidth[DISP_COLUMN_PROFESSION] = 15;  // adjusted in the loop below
     col_minwidth[DISP_COLUMN_LABORS] = num_columns*3/5;     // 60%
     col_maxwidth[DISP_COLUMN_LABORS] = NUM_COLUMNS;
 

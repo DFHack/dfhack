155a4d044c45cf58ccb0bcbbc8168793faa0bb60
expwnent <q309185@gmail.com>
2012-12-14 23:29:28 -0500

    EventManager: fiddled with time events. Made it possible to register for time events before a world is loaded. Also added some files I forgot to add to the previous commit.

--- plugins/eventExample.cpp
+++ plugins/eventExample.cpp
@@ -12,14 +12,20 @@ DFHACK_PLUGIN("eventExample");
 
 void jobInitiated(color_ostream& out, void* job);
 void jobCompleted(color_ostream& out, void* job);
+void timePassed(color_ostream& out, void* ptr);
 
 DFhackCExport command_result plugin_init(color_ostream &out, std::vector<PluginCommand> &commands) {
     EventManager::EventHandler initiateHandler(jobInitiated);
     EventManager::EventHandler completeHandler(jobCompleted);
+    EventManager::EventHandler timeHandler(timePassed);
     Plugin* me = Core::getInstance().getPluginManager()->getPluginByName("eventExample");
 
     EventManager::registerListener(EventManager::EventType::JOB_INITIATED, initiateHandler, me);
     EventManager::registerListener(EventManager::EventType::JOB_COMPLETED, completeHandler, me);
+    EventManager::registerTick(timeHandler, 1, me);
+    EventManager::registerTick(timeHandler, 2, me);
+    EventManager::registerTick(timeHandler, 4, me);
+    EventManager::registerTick(timeHandler, 8, me);
     
     return CR_OK;
 }
@@ -32,3 +38,6 @@ void jobCompleted(color_ostream& out, void* job) {
     out.print("Job completed! 0x%X\n", job);
 }
 
+void timePassed(color_ostream& out, void* ptr) {
+    out.print("Time: %d\n", (int32_t)(ptr));
+}

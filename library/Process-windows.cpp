72eb3669972317d9daec7b8a9a1243cd685a87fd
Quietust <quietust@gmail.com>
2014-02-27 15:58:15 -0600

    Merge changes, also fix Windows build (missing header)

--- library/Process-windows.cpp
+++ library/Process-windows.cpp
@@ -24,8 +24,12 @@ distribution.
 
 #include "Internal.h"
 
+#define _WIN32_WINNT 0x0501
+#define WINVER 0x0501
+
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <psapi.h>
 
 #include <cstring>
 #include <cstdio>

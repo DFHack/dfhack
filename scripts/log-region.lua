6a7abaec72f56f9942c12b90e795433ef804a6d2
PeridexisErrant <PeridexisErrant@gmail.com>
2014-03-22 11:16:06 +1100

    fixes log-region
    stuck in an extra test that `site ~= nil` before writing; which catches
    the error and also confines the script to fort mode - fortunate since
    the output only make sense for fort mode.

--- scripts/log-region.lua
+++ scripts/log-region.lua
@@ -12,22 +12,24 @@ if args[1] == 'disable' then
     dfhack.onStateChange[_ENV] = nil
 else
     dfhack.onStateChange[_ENV] = function(op)
-        if op == SC_WORLD_LOADED then -- there's a problem here - it's fine with fort mode, but loading legends or adventurer returns a nil error as 'site' is a nil value
-            local site = df.world_site.find(df.global.ui.site_id)
-            local fort_ent = df.global.ui.main.fortress_entity
-            local civ_ent = df.historical_entity.find(df.global.ui.civ_id)
-	    local worldname = df.global.world.world_data.name
-            -- site positions
-            -- site  .pos.x  .pos.y
-            -- site  .rgn_min_x  .rgn_min_y  .rgn_max_x  .rgn_max.y
-            -- site  .global_min_x  .global_min_y  .global_max_x  .global_max_y
-            --site.name
-            --fort_ent.name
-            --civ_ent.name
-            
-            write_gamelog('Loaded '..df.global.world.cur_savegame.save_dir..', '..dfhack.TranslateName(worldname)..' ('..dfhack.TranslateName(worldname ,true)..') at coordinates ('..site.pos.x..','..site.pos.y..')'..NEWLINE..
-			  'Loaded the fortress '..dfhack.TranslateName(site.name)..' ('..dfhack.TranslateName(site.name, true)..'), colonized by the group '..dfhack.TranslateName(fort_ent.name)..' ('..dfhack.TranslateName(fort_ent.name,true)..
-                              ') of the civilization '..dfhack.TranslateName(civ_ent.name)..' ('..dfhack.TranslateName(civ_ent.name,true)..').'..NEWLINE)
+        if op == SC_WORLD_LOADED then
+			if df.world_site.find(df.global.ui.site_id) ~= nil then -- added this check, now only attempts write in fort mode
+				local site = df.world_site.find(df.global.ui.site_id)
+				local fort_ent = df.global.ui.main.fortress_entity
+				local civ_ent = df.historical_entity.find(df.global.ui.civ_id)
+			local worldname = df.global.world.world_data.name
+				-- site positions
+				-- site  .pos.x  .pos.y
+				-- site  .rgn_min_x  .rgn_min_y  .rgn_max_x  .rgn_max.y
+				-- site  .global_min_x  .global_min_y  .global_max_x  .global_max_y
+				--site.name
+				--fort_ent.name
+				--civ_ent.name
+				
+				write_gamelog('Loaded '..df.global.world.cur_savegame.save_dir..', '..dfhack.TranslateName(worldname)..' ('..dfhack.TranslateName(worldname ,true)..') at coordinates ('..site.pos.x..','..site.pos.y..')'..NEWLINE..
+				  'Loaded the fortress '..dfhack.TranslateName(site.name)..' ('..dfhack.TranslateName(site.name, true)..'), colonized by the group '..dfhack.TranslateName(fort_ent.name)..' ('..dfhack.TranslateName(fort_ent.name,true)..
+								  ') of the civilization '..dfhack.TranslateName(civ_ent.name)..' ('..dfhack.TranslateName(civ_ent.name,true)..').'..NEWLINE)
+			end
         end
     end
 end

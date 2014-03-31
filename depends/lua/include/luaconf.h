7ea8e25cd38bd2f5a74680e6b8fc62adf59976f2
Alexander Gavrilov <angavrilov@gmail.com>
2014-03-31 13:51:54 +0400

    Rename the LUA_PATH/LUA_CPATH environment variables to avoid conflicts.
    If a user has a standalone lua installation with these variables
    configured, it should not confuse dfhack into looking for things
    in wrong places.

--- depends/lua/include/luaconf.h
+++ depends/lua/include/luaconf.h
@@ -106,6 +106,8 @@
 		LUA_CDIR"?.so;" "./?.so"
 #endif			/* } */
 
+#define LUA_PATH "DFHACK_LUA_PATH"
+#define LUA_CPATH "DFHACK_LUA_CPATH"
 
 /*
 @@ LUA_DIRSEP is the directory separator (for submodules).

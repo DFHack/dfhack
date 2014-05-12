01d302efc3978cb7fa598157822ba5f1ccdf1603
PeridexisErrant <PeridexisErrant@gmail.com>
2014-05-12 10:34:57 +1000

    fixed whitespace again to four-space indentations
    should have read the documentation before trying a style fix...

--- scripts/exportlegends.lua
+++ scripts/exportlegends.lua
@@ -12,57 +12,57 @@ if args[1] then print('   A script which exports data from legends mode.  Valid 
 
 
 local MAPS = {
-        [0] = "Standard biome+site map",
-        "Elevations including lake and ocean floors",
-        "Elevations respecting water level",
-        "Biome",
-        "Hydrosphere",
-        "Temperature",
-        "Rainfall",
-        "Drainage",
-        "Savagery",
-        "Volcanism",
-        "Current vegetation",
-        "Evil",
-        "Salinity",
-        "Structures/fields/roads/etc.",
-        "Trade",
+    [0] = "Standard biome+site map",
+    "Elevations including lake and ocean floors",
+    "Elevations respecting water level",
+    "Biome",
+    "Hydrosphere",
+    "Temperature",
+    "Rainfall",
+    "Drainage",
+    "Savagery",
+    "Volcanism",
+    "Current vegetation",
+    "Evil",
+    "Salinity",
+    "Structures/fields/roads/etc.",
+    "Trade",
 }
 function wait_for_legends_vs()
-        vs = dfhack.gui.getCurViewscreen()
-        if i < 15 then
-                if df.viewscreen_legendsst:is_instance(vs) then
-                        gui.simulateInput(vs, 'LEGENDS_EXPORT_DETAILED_MAP') -- "d" on screen some number internally 
-                        dfhack.timeout(10,'frames',wait_for_export_maps_vs)
-                else
-                        dfhack.timeout(10,'frames',wait_for_legends_vs)
-                end
+    vs = dfhack.gui.getCurViewscreen()
+    if i < 15 then
+        if df.viewscreen_legendsst:is_instance(vs) then
+            gui.simulateInput(vs, 'LEGENDS_EXPORT_DETAILED_MAP') -- "d" on screen some number internally 
+            dfhack.timeout(10,'frames',wait_for_export_maps_vs)
+        else
+            dfhack.timeout(10,'frames',wait_for_legends_vs)
         end
+    end
 end
 function wait_for_export_maps_vs()
-        vs = dfhack.gui.getCurViewscreen()
-        if df.viewscreen_export_graphical_mapst:is_instance(vs) then
-                vs.anon_13 = i -- anon_13 appears to be the selection cursor for this viewscreen
-                print('Exporting:  '..MAPS[i])
-                i = i + 1
-                gui.simulateInput(vs, 'SELECT') -- 1 internally, enter on screen
-                dfhack.timeout(10,'frames',wait_for_legends_vs)
-        else
-                dfhack.timeout(10,'frames',wait_for_export_maps_vs)
-        end
+    vs = dfhack.gui.getCurViewscreen()
+    if df.viewscreen_export_graphical_mapst:is_instance(vs) then
+        vs.anon_13 = i -- anon_13 appears to be the selection cursor for this viewscreen
+        print('Exporting:  '..MAPS[i])
+        i = i + 1
+        gui.simulateInput(vs, 'SELECT') -- 1 internally, enter on screen
+        dfhack.timeout(10,'frames',wait_for_legends_vs)
+    else
+        dfhack.timeout(10,'frames',wait_for_export_maps_vs)
+    end
 end
 
 if df.viewscreen_legendsst:is_instance( vs ) then -- dfhack.gui.getCurFocus() == "legends" 
-        if args[1] == "all" then 
-                gui.simulateInput(vs, df.interface_key.LEGENDS_EXPORT_MAP)
-                print('Exporting:  world map/gen info')
-                gui.simulateInput(vs, df.interface_key.LEGENDS_EXPORT_XML)
-                print('Exporting:  legends xml')
-                wait_for_legends_vs()
-        elseif args[1] == "maps" then wait_for_legends_vs()
-        end
+    if args[1] == "all" then 
+        gui.simulateInput(vs, df.interface_key.LEGENDS_EXPORT_MAP)
+        print('Exporting:  world map/gen info')
+        gui.simulateInput(vs, df.interface_key.LEGENDS_EXPORT_XML)
+        print('Exporting:  legends xml')
+        wait_for_legends_vs()
+    elseif args[1] == "maps" then wait_for_legends_vs()
+    end
 elseif df.viewscreen_export_graphical_mapst:is_instance(vs) then
-        if args[1] == "maps" or "all" then wait_for_export_maps_vs() end
+    if args[1] == "maps" or "all" then wait_for_export_maps_vs() end
 else
-        dfhack.printerr('Not in legends view')
+    dfhack.printerr('Not in legends view')
 end

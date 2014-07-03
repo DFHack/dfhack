7b057629dc9ac36125c8a95a481beaf0d07aa2a6
expwnent <null@nowhere.com>
2014-07-03 16:14:57 -0400

    Tweak anonymous-script.

--- scripts/modtools/anonymous-script.lua
+++ scripts/modtools/anonymous-script.lua
@@ -1,10 +1,12 @@
---anonymous-script.lua
+--scripts/modtools/anonymous-script.lua
 --author expwnent
 --a tool for invoking simple lua scripts without putting them in a file first
---anonymous-script "print(args[1])" arg1 arg2 # prints "arg1"
+--anonymous-script "print(args[1])" arg1 arg2
+--# prints "arg1"
 
 local args = {...}
 
+--automatically collect arguments to make the anonymous script more succinct
 local f,err = load('local args = {...}; ' .. args[1], '=(anonymous lua script)') --,'=(lua command)', 't')
 if err then
  error(err)

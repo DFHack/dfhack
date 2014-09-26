9fbdadfa487b0d013f86585c5586b7ab9648aaeb
hobotron-df <hobotron@netlag.com>
2014-09-25 21:08:32 -0500

    Fixed superdwarf script to use new unit action events.

--- scripts/superdwarf.rb
+++ scripts/superdwarf.rb
@@ -15,10 +15,21 @@ when 'add'
 			else
 				$superdwarf_ids.each { |id|
 					if u = df.unit_find(id) and not u.flags1.dead
-						# faster walk/work
-						if u.counters.job_counter > 0
-							u.counters.job_counter = 0
-						end
+						u.actions.each { |a|
+							case a.type
+							when :Move
+								a.data.move.timer = 1
+							when :Climb,
+								a.data.climb.timer = 1
+							when :Job
+								a.data.job.timer = 1
+							when :Job2
+								a.data.job2.timer = 1
+							when :Attack
+								a.data.attack.timer1 = 1
+								a.data.attack.timer2 = 1
+							end
+						}
 
 						# no sleep
 						if u.counters2.sleepiness_timer > 10000

838bbd97f646ce2be209d0c924b6da8ce2f40fd4
expwnent <null@nowhere.com>
2014-11-09 18:37:11 -0500

    New devel scripts for testing interaction-trigger.

--- scripts/devel/print-args.lua
+++ scripts/devel/print-args.lua
@@ -1,10 +1,8 @@
 --print-args.lua
 --author expwnent
---prints all the arguments on their own line with quotes around them. useful for debugging
+--prints all the arguments on their own line. useful for debugging
 
 local args = {...}
-print("print-args")
 for _,arg in ipairs(args) do
- print("'"..arg.."'")
+ print(arg)
 end
-

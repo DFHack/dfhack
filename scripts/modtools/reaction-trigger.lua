762f5231e2fbb943ebcfac12c4b63be7dd8baa41
expwnent <null@nowhere.com>
2014-11-28 23:01:44 -0500

    reaction-trigger typo.

--- scripts/modtools/reaction-trigger.lua
+++ scripts/modtools/reaction-trigger.lua
@@ -54,7 +54,7 @@ local function processCommand(job, worker, target, building, command)
   if arg == '\\WORKER_ID' then
    table.insert(result,''..worker.id)
   elseif arg == '\\TARGET_ID' then
-   table.insert(restul,''..target.id)
+   table.insert(result,''..target.id)
   elseif arg == '\\BUILDING_ID' then
    table.insert(result,''..building.id)
   elseif arg == '\\LOCATION' then

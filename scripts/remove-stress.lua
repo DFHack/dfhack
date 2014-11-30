81b75161d6f12fd1947cb7123002519ff2e69d43
expwnent <dfexpwnent+github@gmail.com>
2014-11-30 09:23:35 -0500

    Updated remove-stress to the new argument system.

--- scripts/remove-stress.lua
+++ scripts/remove-stress.lua
@@ -3,12 +3,32 @@
 
 --By Putnam; http://www.bay12forums.com/smf/index.php?topic=139553.msg5820486#msg5820486
 
-local args = {...}
+local utils = require 'utils'
 
-if args[1]=='all' then
-    for k,v in ipairs(df.global.world.units.active) do
-        v.status.current_soul.personality.stress_level=-1000000
-    end
+validArgs = validArgs or utils.invert({
+ 'help',
+ 'all'
+})
+
+local args = utils.processArgs({...}, validArgs)
+
+if args.help then
+ print([[
+remove-stress [-all]
+  sets the stress level of every unit to -1000000, or just the selected unit if the '-all' argument is not given
+]])
+ return
+end
+
+if args.all then
+ for k,v in ipairs(df.global.world.units.active) do
+  v.status.current_soul.personality.stress_level=-1000000
+ end
 else
-    dfhack.gui.getSelectedUnit().status.current_soul.personality.stress_level=-1000000
+ local unit = dfhack.gui.getSelectedUnit()
+ if unit then
+  unit.status.current_soul.personality.stress_level=-1000000
+ else
+  error 'Invalid usage: No unit selected and -all argument not given.'
+ end
 end

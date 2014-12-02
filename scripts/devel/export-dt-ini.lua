8ecceb8c67c42ee74cdd3fd9ffc725ea7f6d3784
lethosor <lethosor@gmail.com>
2014-12-02 14:52:12 -0500

    Update devel/export-dt-ini.lua (0.40.19)

--- scripts/devel/export-dt-ini.lua
+++ scripts/devel/export-dt-ini.lua
@@ -306,13 +306,12 @@ address('turn_count',df.unit,'curse','time_on_site')
 address('souls',df.unit,'status','souls')
 address('states',df.unit,'status','misc_traits')
 address('labors',df.unit,'status','labors')
---address('thoughts',df.unit,'status','recent_events')
---address('happiness',df.unit,'status','happiness')
 address('hist_id',df.unit,'hist_figure_id')
 address('artifact_name',df.unit,'status','artifact_name')
 address('active_syndrome_vector',df.unit,'syndromes','active')
 address('syn_sick_flag',df.unit_syndrome,'flags')
 address('unit_health_info',df.unit,'health')
+address('temp_mood',df.unit,'counters','soldier_mood')
 address('counters1',df.unit,'counters','winded')
 address('counters2',df.unit, 'counters','pain')
 address('counters3',df.unit, 'counters2','paralysis')
@@ -358,9 +357,20 @@ address('skills',df.unit_soul,'skills')
 address('preferences',df.unit_soul,'preferences')
 address('personality',df.unit_soul,'personality')
 address('beliefs',df.unit_personality,'values')
+address('emotions',df.unit_personality,'emotions')
 address('goals',df.unit_personality,'dreams')
 address('goal_realized',df.unit_personality.T_dreams,'unk8')
 address('traits',df.unit_personality,'traits')
+address('stress_level',df.unit_personality,'stress_level')
+
+header('emotion_offsets')
+address('emotion_type',df.unit_personality.T_emotions,'emotion_type')
+address('strength',df.unit_personality.T_emotions,'strength')
+address('thought_id',df.unit_personality.T_emotions,'thought')
+address('sub_id',df.unit_personality.T_emotions,'subthought')
+address('level',df.unit_personality.T_emotions,'severity')
+address('year',df.unit_personality.T_emotions,'year')
+address('year_tick',df.unit_personality.T_emotions,'year_tick')
 
 header('job_details')
 address('id',df.job,'job_type')
@@ -426,8 +436,8 @@ size=10
 5\value=0x00020000
 6\name=an invader or hostile
 6\value=0x00080000
-7\name=an invader or hostile
-7\value=0x000C0000
+7\name=resident, invader or ambusher
+7\value=0x00600000
 8\name=part of a merchant caravan
 8\value=0x00000080
 9\name="Dead, Jim."
@@ -443,7 +453,7 @@ size=5
 2\value=0x00040000
 3\name=resident
 3\value=0x00080000
-4\name=visitor_uninvited
+4\name=uninvited visitor
 4\value=0x00400000
 5\name=visitor
 5\value=0x00800000

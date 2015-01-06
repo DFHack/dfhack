5b54909886bdd58087506d4d35c43edc073b3a14
Peridexis Errant <PeridexisErrant@gmail.com>
2015-01-06 22:48:24 +1100

    Move hotkey-notes to scripts, delete needs_porting!
    The notes script is unfinished, but that's the last thing to do - since
    everything else is obsolete, ported, or the subject of a github issue to
    make a replacement.

--- scripts/hotkey-notes.lua
+++ scripts/hotkey-notes.lua
@@ -1,5 +1,9 @@
 -- prints info on assigned hotkeys to the console
 
+-- A work in progress, hoping for some help!
+-- A different approach might be needed depending on internal structures, 
+--    but this gets the idea across.
+
 local hotkeys = {'F1 ', 'F2 ', 'F3 ', 'F4 ', 'F5 ', 'F6 ',
                  'F7 ', 'F8 ', 'F9 ', 'F10', 'F11', 'F12'}
 
@@ -14,27 +18,3 @@ for i=1, #hotkeys do
 
     print(hk.id..'  '..hk.name..'    X= '..hk.x..',  Y= '..hk.y..',  Z= '..hk.z)
 end
-
---[[ 
-# the (very) old Python version...
-from context import Context, ContextManager
-
-cm = ContextManager("Memory.xml")
-df = cm.get_single_context()
-
-df.attach()
-
-gui = df.gui
-
-print "Hotkeys"
-
-hotkeys = gui.read_hotkeys()
-
-for key in hotkeys:
-    print "x: %d\ny: %d\tz: %d\ttext: %s" % (key.x, key.y, key.z, key.name)
-
-df.detach()
-
-print "Done.  Press any key to continue"
-raw_input()
-]]--

9f0558b5471dd364bb3ab3f8ea38baf118ee1444
Peridexis Errant <PeridexisErrant@gmail.com>
2015-01-08 13:10:11 +1100

    Allow for other hotkeys keybinds
    Using this method is more compact and more flexible.  Also tabs to
    spaces...

--- scripts/hotkey-notes.lua
+++ scripts/hotkey-notes.lua
@@ -1,12 +1,9 @@
 -- prints info on assigned hotkeys to the console
 
-local h_list = {'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8',
-                'Shift+F1', 'Shift+F2', 'Shift+F3', 'Shift+F4',
-				'Shift+F5', 'Shift+F6', 'Shift+F7', 'Shift+F8'}
-
 for i=1, #df.global.ui.main.hotkeys do
     local hk = df.global.ui.main.hotkeys[i-1]
-	if hk.cmd ~= -1 then
-		print(h_list[i]..': '..hk.name..':  x='..hk.x..'  y='..hk.y..'  z='..hk.z)
-	end
+    local key = dfhack.screen.getKeyDisplay(df.interface_key.D_HOTKEY1 + i - 1)
+    if hk.cmd ~= -1 then
+        print(key..': '..hk.name..':  x='..hk.x..'  y='..hk.y..'  z='..hk.z)
+    end
 end

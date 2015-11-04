58e6ef6524ca984d4acbaf8e613a8cc86d06b081
PeridexisErrant <PeridexisErrant@gmail.com>
2015-11-04 23:13:52 +1100

    De-alias migrants-now

--- scripts/migrants-now.lua
+++ scripts/migrants-now.lua
@@ -1,11 +1,16 @@
 -- Force a migrant wave (only works after hardcoded waves)
---@alias = 'devel/migrants-now'
 --[[=begin
 
 migrants-now
 ============
 Forces an immediate migrant wave.  Only works after migrants have
-arrived naturally.  Alias for ``devel/migrants-now``; equivalent to
-`modtools/force` ``-eventType migrants``
+arrived naturally.  Equivalent to `modtools/force` ``-eventType migrants``
 
 =end]]
+df.global.timed_events:insert('#',{
+    new = true,
+    type = df.timed_event_type.Migrants,
+    season = df.global.cur_season,
+    season_ticks = df.global.cur_season_tick+1,
+    entity = df.historical_entity.find(df.global.ui.civ_id)
+})

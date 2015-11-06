867b733a0456cb12b9e47215b37a5bf252f4f661
lethosor <lethosor@gmail.com>
2015-11-05 21:48:13 -0500

    Fix a few GCC warnings on Linux (GCC 4.6)

--- library/Hooks-linux.cpp
+++ library/Hooks-linux.cpp
@@ -114,7 +114,8 @@ static int (*_SDL_Init)(uint32_t flags) = 0;
 DFhackCExport int SDL_Init(uint32_t flags)
 {
     // reroute stderr
-    freopen("stderr.log", "w", stderr);
+    if (!freopen("stderr.log", "w", stderr))
+        fprintf(stderr, "dfhack: failed to reroute stderr\n");
     // we don't reroute stdout until  we figure out if this should be done at all
     // See: Console-linux.cpp
 

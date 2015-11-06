29533f1f081cc9bcb401f5b8568eabdc0ad73547
lethosor <lethosor@gmail.com>
2015-11-05 22:57:09 -0500

    Minor sphinx fixes
    * Exclude build directories (rst files are copied into them by CPack)
    * Fix CMake dependency path to conf.py
    * Build in parallel

--- conf.py
+++ conf.py
@@ -173,7 +173,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['docs/_build/*', 'depends/*', 'scripts/3rdparty/*']
+exclude_patterns = ['docs/_build/*', 'depends/*', 'scripts/3rdparty/*', 'build*']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.

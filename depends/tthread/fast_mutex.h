700392ba55b68759e3a1dbdca8bd21470574ecbd
Ben Lubar <ben.lubar@gmail.com>
2016-05-21 19:17:08 -0500

    Fix tinythread memory leak in two ways:
    1. update to tinythread version 1.1, which provides a detach method
    2. move the thread disassociation to the join function, which allows
    attached threads to be joined even after they finish (this was the main
    leak)
    
    Also update RemoteServer.cpp to detach threads and delete the thread
    objects instead of leaking them (although they are much smaller than the
    actual threads).

--- depends/tthread/fast_mutex.h
+++ depends/tthread/fast_mutex.h
@@ -1,5 +1,5 @@
-/*
-Copyright (c) 2010 Marcus Geelnard
+/* -*- mode: c++; tab-width: 2; indent-tabs-mode: nil; -*-
+Copyright (c) 2010-2012 Marcus Geelnard
 
 This software is provided 'as-is', without any express or implied
 warranty. In no event will the authors be held liable for any damages
@@ -47,7 +47,15 @@ freely, subject to the following restrictions:
 #endif
 
 #if defined(_TTHREAD_WIN32_)
+  #ifndef WIN32_LEAN_AND_MEAN
+    #define WIN32_LEAN_AND_MEAN
+    #define __UNDEF_LEAN_AND_MEAN
+  #endif
   #include <windows.h>
+  #ifdef __UNDEF_LEAN_AND_MEAN
+    #undef WIN32_LEAN_AND_MEAN
+    #undef __UNDEF_LEAN_AND_MEAN
+  #endif
 #else
   #ifdef _FAST_MUTEX_ASM_
     #include <sched.h>
@@ -237,3 +245,4 @@ class fast_mutex {
 }
 
 #endif // _FAST_MUTEX_H_
+

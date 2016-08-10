02eef951474871ca9726d5aef300a8ab298c21d3
lethosor <lethosor@gmail.com>
2016-08-09 20:00:31 -0400

    Fix base address for Darwin x64

--- library/include/Memory.h
+++ library/include/Memory.h
@@ -7,7 +7,7 @@
     #endif
 #elif defined(_DARWIN)
     #ifdef DFHACK64
-        #define DEFAULT_BASE_ADDR 0x1000
+        #define DEFAULT_BASE_ADDR 0x100000000
     #else
         #define DEFAULT_BASE_ADDR 0x1000
     #endif

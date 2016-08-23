64e3b12f4fcaa789c447ee1150151e81dd3c648b
lethosor <lethosor@gmail.com>
2016-08-23 15:54:11 -0400

    Make docs/build.sh a bit more configurable

--- docs/build.sh
+++ docs/build.sh
@@ -9,12 +9,12 @@ cd $(dirname "$0")
 cd ..
 
 sphinx=sphinx-build
-if [ -n "$1" ]; then
-    sphinx=$1
+if [ -n "$SPHINX" ]; then
+    sphinx=$SPHINX
 fi
 
 if [ -z "$JOBS" ]; then
     JOBS=2
 fi
 
-"$sphinx" -a -E -q -b html . ./docs/html -w ./docs/_sphinx-warnings.txt -j "$JOBS"
+"$sphinx" -a -E -q -b html . ./docs/html -w ./docs/_sphinx-warnings.txt -j "$JOBS" "$@"

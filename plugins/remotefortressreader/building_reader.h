5ea964b9cf15c970d09dd9b05bdd40e83b93b287
Japa <japa.mala.illo@gmail.com>
2017-03-25 22:23:40 +0530

    Send building items with buildings.

---
+++ plugins/remotefortressreader/building_reader.h
@@ -0,0 +1,9 @@
+#ifndef BUILDING_READER_H
+#define BUILDING_READER_H
+#include "RemoteClient.h"
+#include "RemoteFortressReader.pb.h"
+
+DFHack::command_result GetBuildingDefList(DFHack::color_ostream &stream, const DFHack::EmptyMessage *in, RemoteFortressReader::BuildingList *out);
+void CopyBuilding(int buildingIndex, RemoteFortressReader::BuildingInstance * remote_build);
+
+#endif

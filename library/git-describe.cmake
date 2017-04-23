9d5b69cb20c08a88306232380370a11439cc304c
lethosor <lethosor@gmail.com>
2017-04-23 18:16:00 -0400

    git-describe.cmake: use 8-digit git commit abbreviations
    The length here was previously determined automatically, and jumped from 7 to 8
    recently, breaking devel/check-release. Since Git seems to think 8 characters is
    better now, specify it explicitly to avoid future breakages.

--- library/git-describe.cmake
+++ library/git-describe.cmake
@@ -1,10 +1,10 @@
-execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags --long
+execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags --abbrev=8 --long
     WORKING_DIRECTORY "${dfhack_SOURCE_DIR}"
     OUTPUT_VARIABLE DFHACK_GIT_DESCRIPTION)
 execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
     WORKING_DIRECTORY "${dfhack_SOURCE_DIR}"
     OUTPUT_VARIABLE DFHACK_GIT_COMMIT)
-execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags --exact-match
+execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags --abbrev=8 --exact-match
     WORKING_DIRECTORY "${dfhack_SOURCE_DIR}"
     RESULT_VARIABLE DFHACK_GIT_TAGGED_RESULT
     OUTPUT_QUIET ERROR_QUIET)

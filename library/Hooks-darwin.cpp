dff94fc97e05d66bdbb7ab92a4ced31e63adf8c2
lethosor <lethosor@gmail.com>
2017-06-10 19:41:59 -0400

    replacment -> replacement

--- library/Hooks-darwin.cpp
+++ library/Hooks-darwin.cpp
@@ -59,10 +59,10 @@ typedef struct interpose_s
 
 };*/
 
-#define DYLD_INTERPOSE(_replacment,_replacee) \
+#define DYLD_INTERPOSE(_replacement,_replacee) \
     __attribute__((used)) static struct{ const void* replacment; const void* replacee; } \
     _interpose_##_replacee __attribute__ ((section ("__DATA,__interpose"))) = \
-    { (const void*)(unsigned long)&_replacment, (const void*)(unsigned long)&_replacee };
+    { (const void*)(unsigned long)&_replacement, (const void*)(unsigned long)&_replacee };
 
 DYLD_INTERPOSE(DFH_SDL_Init,SDL_Init);
 DYLD_INTERPOSE(DFH_SDL_PollEvent,SDL_PollEvent);

fd19935fe95d5150168d1db48292d01961291da1
Kelly Kinkade <kelly.lynn.martin@gmail.com>
2017-07-22 04:54:05 -0500

    labormanager: remove stupid

--- plugins/labormanager_joblabormapper.h
+++ plugins/labormanager_joblabormapper.h
@@ -9,7 +9,7 @@ private:
     std::map<df::job_type, jlfunc*> job_to_labor_table;
     std::map<df::unit_labor, jlfunc*> jlf_cache;
 
-    jlfunc* JobLaborMapper::jlf_const(df::unit_labor l);
+    jlfunc* jlf_const(df::unit_labor l);
 
 public:
     ~JobLaborMapper();

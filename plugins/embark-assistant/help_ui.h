80fe49b339e571eb3fe11a644608657225bba7fe
PatrikLundell <j.patrik.r.lundell@gmail.com>
2017-09-01 14:13:34 +0200

    Add embark-assistant plugin

---
+++ plugins/embark-assistant/help_ui.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "PluginManager.h"
+
+#include "DataDefs.h"
+
+#include "defs.h"
+
+using namespace DFHack;
+
+namespace embark_assist {
+    namespace help_ui {
+        void init(DFHack::Plugin *plugin_self);
+    }
+}

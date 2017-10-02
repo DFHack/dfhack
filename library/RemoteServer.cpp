cbeb2414359ca801e1806f874929a86309d07bdf
Japa <japa.mala.illo@gmail.com>
2017-10-02 20:48:05 +0530

    Listen on any address

--- library/RemoteServer.cpp
+++ library/RemoteServer.cpp
@@ -379,7 +379,7 @@ bool ServerMain::listen(int port)
 
     socket->Initialize();
 
-    if (!socket->Listen("127.0.0.1", port))
+    if (!socket->Listen(NULL, port))
         return false;
 
     thread = new tthread::thread(threadFn, this);

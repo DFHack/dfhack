3351d78864a9b62933474b522d2719fec59b8abd
Kelly Kinkade <kelly.lynn.martin@gmail.com>
2018-09-11 18:27:44 -0500

    Change embark-assistant to biome type from Maps
    See #1392

--- plugins/embark-assistant/survey.cpp
+++ plugins/embark-assistant/survey.cpp
@@ -8,6 +8,7 @@
 
 #include <modules/Gui.h>
 #include "modules/Materials.h"
+#include "modules/Maps.h"
 
 #include "DataDefs.h"
 #include "df/coord2d.h"
@@ -51,7 +52,6 @@
 #include "df/world_site_type.h"
 #include "df/world_underground_region.h"
 
-#include "biome_type.h"
 #include "defs.h"
 #include "survey.h"
 
@@ -552,7 +552,7 @@ void embark_assist::survey::high_level_world_survey(embark_assist::defs::geo_dat
                     offset_count++;
 
                     results.biome_index[l] = world_data->region_map[adjusted.x][adjusted.y].region_id;
-                    results.biome[l] = get_biome_type(adjusted.x, adjusted.y, k);
+                    results.biome[l] = DFHack::Maps::GetBiomeTypeWithRef(adjusted.x, adjusted.y, k);
                     temperature = world_data->region_map[adjusted.x][adjusted.y].temperature;
                     negative = temperature < 0;
 

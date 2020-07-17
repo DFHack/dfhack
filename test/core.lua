56e43a0dde023c5a4595a22b29d800153b31e3c4
lethosor <lethosor@gmail.com>
2020-07-16 23:19:38 -0400

    Linux: make Process::getPath (and dfhack.getDFPath()) not depend on cwd, for consistency with other platforms

---
+++ test/core.lua
@@ -0,0 +1,34 @@
+local function clean_path(p)
+    -- todo: replace with dfhack.filesystem call?
+    return p:gsub('\\', '/'):gsub('//', '/'):gsub('/$', '')
+end
+
+local fs = dfhack.filesystem
+local old_cwd = clean_path(fs.getcwd())
+local function restore_cwd()
+    fs.chdir(old_cwd)
+end
+
+function test.getDFPath()
+    expect.eq(clean_path(dfhack.getDFPath()), old_cwd)
+end
+
+function test.getDFPath_chdir()
+    dfhack.with_finalize(restore_cwd, function()
+        fs.chdir('data')
+        expect.eq(clean_path(dfhack.getDFPath()), old_cwd)
+        expect.ne(clean_path(dfhack.getDFPath()), clean_path(fs.getcwd()))
+    end)
+end
+
+function test.getHackPath()
+    expect.eq(clean_path(dfhack.getHackPath()), clean_path(dfhack.getDFPath() .. '/hack/'))
+end
+
+function test.getHackPath_chdir()
+    dfhack.with_finalize(restore_cwd, function()
+        fs.chdir('hack')
+        expect.eq(clean_path(dfhack.getHackPath()), clean_path(old_cwd .. '/hack/'))
+        expect.eq(clean_path(dfhack.getHackPath()), clean_path(fs.getcwd()))
+    end)
+end

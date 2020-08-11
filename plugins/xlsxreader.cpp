cfc736caa1d627c800cee49221d30945bfc8567d
Myk Taylor <myk.taylor@gmail.com>
2020-08-11 14:18:09 -0700

    give the identity classes distinctive name fields
    both would have been represented as "handle" to Lua

--- plugins/xlsxreader.cpp
+++ plugins/xlsxreader.cpp
@@ -15,13 +15,13 @@ DFHACK_PLUGIN("xlsxreader");
 
 struct xlsx_file_handle_identity : public compound_identity {
     xlsx_file_handle_identity()
-        :compound_identity(0, nullptr, nullptr, "handle") {};
+        :compound_identity(0, nullptr, nullptr, "xlsx_file_handle") {};
     DFHack::identity_type type() override { return IDTYPE_OPAQUE; }
 };
 
 struct xlsx_sheet_handle_identity : public compound_identity {
     xlsx_sheet_handle_identity()
-        :compound_identity(0, nullptr, nullptr, "handle") {};
+        :compound_identity(0, nullptr, nullptr, "xlsx_sheet_handle") {};
     DFHack::identity_type type() override { return IDTYPE_OPAQUE; }
 };
 

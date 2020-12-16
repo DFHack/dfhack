371314d9c3677586ffb277107463fd6ca9ea6550
lethosor <lethosor@gmail.com>
2020-12-16 13:16:04 -0500

    Update scripts, tweak test

--- test/plugins/workflow.lua
+++ test/plugins/workflow.lua
@@ -2,6 +2,6 @@ local workflow = require('plugins.workflow')
 
 function test.job_outputs()
     for job_type in pairs(workflow.test_data.job_outputs) do
-        expect.true_(df.job_type[job_type], "Found unrecognized job type: " .. tostring(job_type))
+        expect.ne(df.job_type[job_type], nil, "Found unrecognized job type: " .. tostring(job_type))
     end
 end

fd7c3fc4aae75338ab03d41dab286fb8819e0206
lethosor <lethosor@gmail.com>
2021-01-30 21:12:06 -0500

    Prevent stockpiles hotkey label from overlapping with autodump
    stockflow moved to last slot because it isn't enabled in dfhack.init-example
    
    Ref #1764

--- plugins/stockflow.cpp
+++ plugins/stockflow.cpp
@@ -212,7 +212,7 @@ public:
         auto dims = Gui::getDwarfmodeViewDims();
         int left_margin = dims.menu_x1 + 1;
         int x = left_margin;
-        int y = dims.y2 - 3;
+        int y = dims.y2 - 2; // below autodump, automelt, autotrade, stocks, stockpiles
 
         int links = 0;
         links += sp->links.give_to_pile.size();
--- plugins/stockpiles/stockpiles.cpp
+++ plugins/stockpiles/stockpiles.cpp
@@ -409,7 +409,7 @@ struct stockpiles_import_hook : public df::viewscreen_dwarfmodest
         auto dims = Gui::getDwarfmodeViewDims();
         int left_margin = dims.menu_x1 + 1;
         int x = left_margin;
-        int y = dims.y2 - 7; // above automelt, autotrade, stocks, and stockflow
+        int y = dims.y2 - 3; // below autodump, automelt, autotrade, stocks; above stockflow
         OutputHotkeyString ( x, y, "Load/Save Settings", "l", true, left_margin, COLOR_WHITE, COLOR_LIGHTRED );
     }
 };

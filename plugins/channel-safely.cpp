3dd9beca881e669adfce1069a7088232b10ed744
Josh Cooper <cooper.s.josh@outlook.com>
2021-06-20 05:57:26 -0700

    Rewrites most of plugin/channel-safely

--- plugins/channel-safely.cpp
+++ plugins/channel-safely.cpp
@@ -32,17 +32,17 @@ DECLARE_HASA(when) //declares above statement with 'when' replacing 'what'
 // end usage is: `has_when<T>::value`
 // the only use is to allow reliance on pull request #1876 which introduces a refactor which prevents some manual management
 
+ChannelManager manager;
 void onTick(color_ostream &out, void* tick_ptr);
 void onStart(color_ostream &out, void* job);
 void onComplete(color_ostream &out, void* job);
 
+void manageNeighbours(const df::coord &tile);
 void cancelJob(df::job* job);
+bool is_group_done(const GroupData::Group &group);
 bool is_dig(df::job* job);
-bool is_dig(df::tile_designation &designation);
 bool is_channel(df::job* job);
 bool is_channel(df::tile_designation &designation);
-bool is_designated(df::tile_designation &designation);
-bool is_marked(df::tile_occupancy &occupancy);
 
 
 command_result manage_channel_designations (color_ostream &out, std::vector <std::string> &parameters);
@@ -71,6 +71,8 @@ DFhackCExport command_result plugin_enable(color_ostream &out, bool enable) {
         }
         EM::registerListener(EventType::JOB_INITIATED, jobStartHandler, plugin_self);
         EM::registerListener(EventType::JOB_COMPLETED, jobCompletionHandler, plugin_self);
+        manager.build_groups();
+        manager.manage_all_designations(out);
     } else if (!enable) {
         EM::unregisterAll(plugin_self);
     }
@@ -85,6 +87,7 @@ DFhackCExport command_result plugin_shutdown(color_ostream &out) {
     return CR_OK;
 }
 
+// todo: figure out if useful to perhaps rebuild group data
 //DFhackCExport command_result plugin_onupdate(color_ostream &out) {
 //    static uint64_t count = 0;
 //    out.print("onupdate() - %d\n", ++count);
@@ -93,9 +96,9 @@ DFhackCExport command_result plugin_shutdown(color_ostream &out) {
 
 command_result manage_channel_designations(color_ostream &out, std::vector<std::string> &parameters){
     if(parameters.empty()){
-        out.print("Checking for unsafe designations!\n");
-        ChannelManager m;
-        m.manage_designations(out);
+        out.print("Work safe inspection! Now checking for unsafe work conditions..\n");
+        manager.build_groups();
+        manager.manage_all_designations(out);
     } else if (parameters.size() == 1 && parameters[0] == "enable") {
         if (plugin_enable(out, true) == CR_OK){
             out.print("channel-safely enabled!\n");
@@ -111,16 +114,14 @@ command_result manage_channel_designations(color_ostream &out, std::vector<std::
 }
 
 void onTick(color_ostream &out, void* tick_ptr){
-    out.print("the only once onTick()! Come one, come ALL!\n");
     if(enabled && World::isFortressMode()) {
         static uint64_t count = 0;
         static int32_t last_tick_counter = 0;
         int32_t tick_counter = (int32_t) ((intptr_t) tick_ptr);
-        out.print("onTick() - %d, %d\n", ++count, tick_counter);
+        //out.print("onTick() - %d, %d\n", ++count, tick_counter);
         if ((tick_counter - last_tick_counter) >= tickFreq) {
             last_tick_counter = tick_counter;
-            ChannelManager m;
-            m.manage_designations(out);
+            manager.build_groups();
         }
     }
     namespace EM = EventManager;
@@ -133,123 +134,129 @@ void onTick(color_ostream &out, void* tick_ptr){
 
 void onStart(color_ostream &out, void* job_ptr){
     if(enabled && World::isFortressMode()) {
-        df::job* job = (df::job*) job_ptr;
+        auto job = (df::job*) job_ptr;
         if (is_dig(job) || is_channel(job)) {
-            // channeling from above would be unsafe
-            df::coord pos = job->pos;
-            pos.z += 1;
-            df::tile_designation &aboved = *Maps::getTileDesignation(pos);
-            if (is_channel(aboved)) {
-                cancelJob(job);
-                df::tile_occupancy &job_o = *Maps::getTileOccupancy(pos);
-                job_o.bits.dig_marked = true;
-            }
+            df::coord local(job->pos);
+            local.x = local.x % 16;
+            local.y = local.y % 16;
+            df::coord above(job->pos);
+            above.z++;
+            df::map_block* block = Maps::getTileBlock(job->pos);
+            //postpone job if above isn't done
+            manager.manage_safety(block, local, job->pos, above);
         }
     }
 }
 
 void onComplete(color_ostream &out, void* job_ptr){
     if(enabled && World::isFortressMode()) {
-        df::job* job = (df::job*) job_ptr;
-        if (is_dig(job) || is_channel(job)) {
-            //above isn't safe to channel
-            df::coord pos = job->pos;
-            pos.z += 1;
-            df::tile_designation &aboved = *Maps::getTileDesignation(pos);
-            if (is_channel(aboved)) {
-                df::tile_occupancy &aboveo = *Maps::getTileOccupancy(pos);
-                aboveo.bits.dig_marked = true;
-            }
-            //below is safe to do anything
-            pos.z -= 2;
-            df::tile_designation &belowd = *Maps::getTileDesignation(pos);
-            if (is_channel(belowd) || is_dig(belowd)) {
-                df::tile_occupancy &belowo = *Maps::getTileOccupancy(pos);
-                //todo: belowo.bits.dig_marked = false;
+        auto job = (df::job*) job_ptr;
+        if (is_channel(job)) {
+            df::coord local(job->pos);
+            local.x = local.x % 16;
+            local.y = local.y % 16;
+            df::coord below(job->pos);
+            below.z--;
+            df::map_block* block = Maps::getTileBlock(below);
+            //activate designation below if group is done now, postpone if not
+            manager.manage_safety(block, local, below, job->pos);
+            manager.mark_done(job->pos);
+            manageNeighbours(job->pos);
+        }
+    }
+}
+
+void ChannelManager::manage_all_designations(color_ostream &out) {
+    static std::once_flag getMapSize;
+    static uint32_t t1, t2, zmax;
+    std::call_once(getMapSize, Maps::getSize, t1, t2, zmax);
+    for(auto &map_pair : groups){
+        GroupData::Group group = map_pair.second;
+        for(auto &group_pair : group){
+            const df::coord &world_pos = group_pair.first;
+            df::map_block* block = group_pair.second;
+            df::coord local(world_pos);
+            local.x = local.x % 16;
+            local.y = local.y % 16;
+            if(world_pos.z < zmax - 1){
+                df::coord above(world_pos);
+                above.z++;
+                manage_safety(block, local, world_pos, above);
+            } else {
+                block->occupancy[local.x][local.y].bits.dig_marked = false;
             }
         }
     }
 }
 
-void ChannelManager::manage_designations(color_ostream &out) {
-//    out.print("isValid: %d\nworld: %d\nmap: %d\n", Maps::IsValid(), world, &world->map);
-//    out.print("map count block (%d, %d, %d)\n", world->map.x_count, world->map.y_count, world->map.z_count);
-//    out.print("map block index (%d, %d, %d)\n", world->map.x_count, world->map.y_count, world->map.z_count);
-    dig_jobs.clear();
-    find_dig_jobs();
-    foreach_block_column(out);
-}
+//#include <df/block_square_event.h>
+#include <df/block_square_event_designation_priorityst.h>
 
-df::job* ChannelManager::find_job(df::coord &tile_pos) {
-    auto iter = dig_jobs.lower_bound(tile_pos.z);
-    while (iter != dig_jobs.end()) {
-        df::coord &pos = iter->second->pos;
-        if (pos == tile_pos) {
-            return iter->second;
-        }
-        iter++;
-    }
-    return nullptr;
-}
-
-void ChannelManager::foreach_block_column(color_ostream &out) {
-    uint32_t x, y, z;
-    Maps::getSize(x, y, z);
-    //out.print("map size in blocks: %d, %d, %d\n", x,y,z);
-    for (int ix = 0; ix < x; ++ix) {
-        for (int iy = 0; iy < y; ++iy) {
-            for (int iz = z - 1; iz > 0; --iz) {
-                df::map_block* top = Maps::getBlock(ix, iy, iz);
-                df::map_block* bottom = Maps::getBlock(ix, iy, iz - 1);
-                if(top && bottom && (top->flags.bits.designated || bottom->flags.bits.designated)) {
-                    foreach_tile(out, top, bottom);
+void ChannelManager::manage_safety(df::map_block* block, const df::coord &local, const df::coord &tile, const df::coord &tile_above) {
+    auto iter = groups.find(tile_above);
+    df::tile_occupancy &tile_occupancy = block->occupancy[local.x][local.y];
+    for(df::block_square_event* event : block->block_events){
+        switch(event->getType()){
+            case block_square_event_type::designation_priority:
+                auto evT = (df::block_square_event_designation_priorityst*)event;
+                if(evT->priority[local.x][local.y] < 6000) {
+                    if (iter != groups.end()) {
+                        GroupData::Group group = iter->second;
+                        if (is_group_done(group)) {
+                            tile_occupancy.bits.dig_marked = false;
+                        } else {
+                            // not safe
+                            tile_occupancy.bits.dig_marked = true;
+                            jobs.cancel_job(tile); //cancels job if designation is an open/active job
+                        }
+                    }
+                    else {
+                        // no group above tile
+                        tile_occupancy.bits.dig_marked = false;
+                    }
                 }
-            }
+                break;
         }
     }
 }
 
-void ChannelManager::foreach_tile(color_ostream &out, df::map_block* top, df::map_block* bottom) {
-    /** Safety checks
-     * is the top tile being channeled or designated to be channeled
-     * - Yes
-     *      is the bottom tile being dug or designated and not marked
-     *      - Yes
-     *           try to cancelJob
-     *           mark it for later
-     * - No
-     *      is the bottom tile designated
-     *      - Yes
-     *           unmark for later
-     */
-    for (int x = 0; x < 16; ++x) {
-        for (int y = 0; y < 16; ++y) {
-            auto &top_d = top->designation[x][y];
-            auto &top_o = top->occupancy[x][y];
-            auto &bottom_d = bottom->designation[x][y];
-            auto &bottom_o = bottom->occupancy[x][y];
-            df::job* top_job = find_job(top->map_pos);
-            df::job* bottom_job = find_job(bottom->map_pos);
-            if (top_job || is_channel(top_d)) {
-                if (bottom_job || (!is_marked(bottom_o) && is_designated(bottom_d))) {
-                    cancelJob(bottom_job);
-                    bottom_o.bits.dig_marked = true;
-                }
-            } else if (is_marked(bottom_o) && is_channel(bottom_d)) {
-                bottom_o.bits.dig_marked = false;
-            }
-        }
+void ChannelManager::mark_done(const df::coord &tile) {
+    auto iter = groups.find(tile);
+    if(iter != groups.end()){
+        GroupData::Group group = iter->second;
+        df::map_block* block = Maps::getTileBlock(tile);
+        group.erase(std::make_pair(tile,block));
     }
 }
 
-void ChannelManager::find_dig_jobs() {
-    df::job_list_link* p = df::global::world->jobs.list.next;
-    while (p) {
-        df::job* job = p->item;
-        p = p->next;
-        if (is_dig(job) || is_channel(job)) {
-            dig_jobs.emplace(job->pos.z, job);
-        }
+void manageNeighbours(const df::coord &tile){
+    df::coord neighbors[8];
+    neighbors[0] = tile;
+    neighbors[1] = tile;
+    neighbors[2] = tile;
+    neighbors[3] = tile;
+    neighbors[4] = tile;
+    neighbors[5] = tile;
+    neighbors[6] = tile;
+    neighbors[7] = tile;
+    neighbors[0].x--; neighbors[0].y--;
+    neighbors[1].x++; neighbors[1].y++;
+    neighbors[2].x++; neighbors[2].y--;
+    neighbors[3].x--; neighbors[3].y++;
+    neighbors[4].x++;
+    neighbors[5].x--;
+    neighbors[6].y--;
+    neighbors[7].y++;
+
+    for(auto &position : neighbors){
+        df::coord local(position);
+        local.x = local.x % 16;
+        local.y = local.y % 16;
+        df::coord above(position);
+        above.z++;
+        df::map_block* block = Maps::getTileBlock(position);
+        //activate designation if group is done now, postpone if not
+        manager.manage_safety(block, local, position, above);
     }
 }
 
@@ -267,12 +274,12 @@ void cancelJob(df::job* job) {
     }
 }
 
-bool is_dig(df::job* job) {
-    return job->job_type == df::job_type::Dig;
+bool is_group_done(const GroupData::Group &group){
+    return group.empty();
 }
 
-bool is_dig(df::tile_designation &designation) {
-    return designation.bits.dig == df::tile_dig_designation::Default;
+bool is_dig(df::job* job) {
+    return job->job_type == df::job_type::Dig;
 }
 
 bool is_channel(df::job* job){
@@ -283,10 +290,97 @@ bool is_channel(df::tile_designation &designation){
     return designation.bits.dig == df::tile_dig_designation::Channel;
 }
 
-bool is_designated(df::tile_designation &designation){
-    return is_dig(designation) || is_channel(designation);
+void GroupData::read(){
+    groups.clear();
+    foreach_block();
+    jobs.read();
+    for(auto &map_entry : jobs){
+        df::job* job = map_entry.second;
+        if(is_channel(job)){
+            add(map_entry.first, Maps::getTileBlock(job->pos));
+        }
+    }
 }
 
-bool is_marked(df::tile_occupancy &occupancy){
-    return occupancy.bits.dig_marked;
+void GroupData::foreach_block() {
+    static std::once_flag getMapSize;
+    static uint32_t x, y, z;
+    std::call_once(getMapSize, Maps::getSize, x, y, z);
+    for (int ix = 0; ix < x; ++ix) {
+        for (int iy = 0; iy < y; ++iy) {
+            for (int iz = z - 1; iz >= 0; --iz) {
+                df::map_block* block = Maps::getBlock(ix, iy, iz);
+                if(block->flags.bits.designated) {
+                    foreach_tile(block, iz);
+                }
+            }
+        }
+    }
+}
+
+void GroupData::foreach_tile(df::map_block* block, int z) {
+    for (int16_t local_x = 0; local_x < 16; ++local_x) {
+        for (int16_t local_y = 0; local_y < 16; ++local_y) {
+            if(is_channel(block->designation[local_x][local_y])){
+                df::coord world_pos(block->map_pos);
+                world_pos.x = local_x + (world_pos.x << 4);
+                world_pos.y = local_y + (world_pos.y << 4);
+                world_pos.z = z;
+                add(world_pos, block);
+            }
+        }
+    }
+}
+
+void GroupData::add(df::coord pos, df::map_block* block) {
+    df::coord neighbors[8];
+    neighbors[0] = pos;
+    neighbors[1] = pos;
+    neighbors[2] = pos;
+    neighbors[3] = pos;
+    neighbors[4] = pos;
+    neighbors[5] = pos;
+    neighbors[6] = pos;
+    neighbors[7] = pos;
+    neighbors[0].x--; neighbors[0].y--;
+    neighbors[1].x++; neighbors[1].y++;
+    neighbors[2].x++; neighbors[2].y--;
+    neighbors[3].x--; neighbors[3].y++;
+    neighbors[4].x++;
+    neighbors[5].x--;
+    neighbors[6].y--;
+    neighbors[7].y++;
+
+    Group return_group;
+    return_group.insert(std::make_pair(pos, block));
+    for(auto &position : neighbors){
+        auto group_iter = groups.find(position);
+        if(group_iter != groups.end()){
+            Group &group = group_iter->second;
+            return_group.insert(group.begin(), group.end());
+        }
+    }
+    for(auto &pair : return_group){
+        groups.emplace(pair.first, return_group);
+    }
+}
+
+void DigJobs::read() {
+    jobs.clear();
+    df::job_list_link* node = df::global::world->jobs.list.next;
+    while (node) {
+        df::job* job = node->item;
+        node = node->next;
+        if (is_channel(job)) {
+            jobs.emplace(job->pos, job);
+        }
+    }
+}
+
+void DigJobs::cancel_job(const df::coord &pos) {
+    auto iter = jobs.find(pos);
+    if(iter != jobs.end()){
+        df::job* job = iter->second;
+        cancelJob(job);
+    }
 }
--- plugins/channel-safely.h
+++ plugins/channel-safely.h
@@ -6,6 +6,7 @@
 #include <PluginManager.h>
 #include <modules/World.h>
 #include <modules/Maps.h>
+#include <modules/MapCache.h>
 #include <modules/Job.h>
 #include <df/map_block.h>
 #include <df/world.h>
@@ -14,40 +15,57 @@
 
 using namespace DFHack;
 
-struct dig_job{
-    enum {
-        NONE,
-        JOB,
-        DESIGNATED
-    } type;
-    union{
-        df::job* job;
-        df::map_block* block;
-    } pointer;
-    df::coord map_pos;
-
-    dig_job(df::job* job){
-        type = JOB;
-        pointer.job = job;
-        map_pos = job->pos;
-    }
-    dig_job(df::map_block* block, df::coord pos){
-        type = DESIGNATED;
-        pointer.block = block;
-        map_pos = pos;
-    }
+// Used to read/store/iterate channel digging jobs
+class DigJobs {
+private:
+    friend class GroupData;
+    using Jobs = std::map<df::coord, df::job*>;
+    Jobs jobs;
+public:
+    void read();
+    void clear() { jobs.clear(); }
+    void cancel_job(const df::coord &pos);
+    Jobs::const_iterator find(const df::coord &pos) const { return jobs.find(pos); }
+    Jobs::const_iterator begin() const { return jobs.begin(); }
+    Jobs::const_iterator end() const { return jobs.end(); }
 };
 
-class ChannelManager {
+// Used to build groups of adjacent channel designations/jobs
+class GroupData {
 public:
-    void manage_designations(color_ostream &out);
-
+    using Group = std::set<std::pair<df::coord, df::map_block*>>;
 private:
-    std::multimap<int16_t, df::job*> dig_jobs;
-
+    using Groups = std::map<df::coord, Group>;
+    Groups groups;
+    DigJobs &jobs;
 protected:
-    df::job* find_job(df::coord &tile_pos);
-    void foreach_block_column(color_ostream &out);
-    void foreach_tile(color_ostream &out, df::map_block* top, df::map_block* bottom);
-    void find_dig_jobs();
+    void foreach_block();
+    void foreach_tile(df::map_block* block, int z);
+    void add(df::coord pos, df::map_block* block);
+public:
+    GroupData(DigJobs &jobs) : jobs(jobs) {}
+    void read();
+    void erase(const df::coord &tile) {
+        auto iter = groups.find(tile);
+        if(iter != groups.end()){
+
+        }
+    }
+    void cancel_job(const df::coord &pos) { jobs.cancel_job(pos); }
+    void clear() { groups.clear(); }
+    Groups::const_iterator find(const df::coord &pos) const { return groups.find(pos); }
+    Groups::const_iterator begin() const { return groups.begin(); }
+    Groups::const_iterator end() const { return groups.end(); }
+};
+
+// Uses GroupData to detect an unsafe work environment
+class ChannelManager {
+private:
+    DigJobs jobs;
+    GroupData groups = GroupData(jobs);
+public:
+    void build_groups() { groups.read(); }
+    void manage_all_designations(color_ostream &out);
+    void manage_safety(df::map_block* block, const df::coord &local, const df::coord &tile, const df::coord &tile_above);
+    void mark_done(const df::coord &tile);
 };

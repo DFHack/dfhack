93d67dd51e881960bdd270a86a74607c6cd7f3c1
myk002 <myk002@yahoo.com>
2021-06-27 19:31:05 -0700

    export dig_now_tile over Lua

--- plugins/dig-now.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "ColorText.h"
-#include "modules/Maps.h"
-
-/**
- * Runs dig-now for the specified tile. Default options apply.
- */
-bool dig_now_tile(DFHack::color_ostream &out, const DFHack::DFCoord &pos);

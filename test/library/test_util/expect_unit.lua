b2f44f00f1b38492808e81a2b11bf78bbd694998
myk002 <myk002@yahoo.com>
2021-08-23 22:59:30 -0700

    add pattern matching expect.find() for unit tests
    settled on `find()` since the string method of the same name has the
    same semantics. other options: `str_find()` since it fails on non-string
    or `match()` (but that might imply that the pattern needs to match the
    entire string).

--- test/library/test_util/expect_unit.lua
+++ test/library/test_util/expect_unit.lua
@@ -1,5 +1,22 @@
 local expect_raw = require('test_util.expect')
 
+function test.find()
+    expect.true_(expect_raw.find('a ', 'a str', 'a comment'))
+
+    local ok, comment, msg = expect_raw.find('ab', 'a str', 'a comment')
+    expect.false_(ok)
+    expect.eq('a comment', comment)
+    expect.eq('pattern "ab" not matched in "a str"', msg)
+
+    ok, _, msg = expect_raw.find('pattern', nil)
+    expect.false_(ok)
+    expect.eq('expected string, got nil', msg)
+
+    ok, _, msg = expect_raw.find('pattern', {})
+    expect.false_(ok)
+    expect.eq('expected string, got table', msg)
+end
+
 function test.table_eq()
     expect.true_(expect_raw.table_eq({}, {}))
     expect.true_(expect_raw.table_eq({'a'}, {'a'}))

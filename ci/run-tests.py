1d0985d51ad9d27e4e8c8db887c7f75dc42d6113
lethosor <lethosor@gmail.com>
2021-09-17 17:42:29 -0400

    Also log complete raw stderr to file

--- ci/run-tests.py
+++ ci/run-tests.py
@@ -115,9 +115,11 @@ try:
         out, err = process.communicate()
         if err:
             print('WARN: DF produced stderr: ' + repr(err[:5000]))
+            with open('df-raw-stderr.log', 'wb') as f:
+                f.write(err)
         if process.returncode != 0:
             print('ERROR: DF exited with ' + repr(process.returncode))
-            print('DF stdout: %r' + repr(out[:5000]))
+            print('DF stdout: ' + repr(out[:5000]))
 finally:
     print('\nRestoring original init.txt')
     shutil.copyfile(init_txt_path + '.orig', init_txt_path)

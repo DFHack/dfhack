dbd6f5ceee356a31d98490b60f043e950be8e73e
Tim Siegel <siegeltr@gmail.com>
2022-04-03 22:58:42 -0400

    [tweak] Fix stable-cursor when no menu at right map edge
    When the view is near the right edge of the map, and the default menu is
    off, the view never stays exactly at the right edge when the menu
    disappears. It is always shifted over half the menu's width from the
    right edge.
    
    Finding the right value involves duplicating some functionality from
    Gui::getDwarfmodeViewDims(), because that hook is cached and does not
    update fully during this INTERPOSE hook. In particular, getting the
    actual viewport width (accounting for area map & menu).

--- plugins/tweak/tweaks/stable-cursor.h
+++ plugins/tweak/tweaks/stable-cursor.h
@@ -38,9 +38,22 @@ struct stable_cursor_hook : df::viewscreen_dwarfmodest
         return ui_menu_width && (*ui_menu_width)[0] < (*ui_menu_width)[1];
     }
 
+    bool check_area()
+    {
+        return ui_menu_width && (*ui_menu_width)[1] == 2;
+    }
+
     DEFINE_VMETHOD_INTERPOSE(void, feed, (set<df::interface_key> *input))
     {
+        uint32_t mx, my, mz;
+        Maps::getTileSize(mx, my, mz);
+        df::coord map(mx, my, mz);
+
+        // Full window has a 1-cell border on each side
+        int16_t width = Screen::getWindowSize().x - 2;
+
         bool was_default = check_default();
+        //bool was_menu = check_menu();
         df::coord view = Gui::getViewportPos();
         df::coord cursor = Gui::getCursorPos();
 
@@ -48,29 +61,40 @@ struct stable_cursor_hook : df::viewscreen_dwarfmodest
 
         bool is_default = check_default();
         bool is_menu = check_menu();
+        //df::coord cur_view = Gui::getViewportPos();
         df::coord cur_cursor = Gui::getCursorPos();
 
         if (is_default && !was_default)
         {
             if (!is_menu)
             {
-                // The menu disappeared, but getViewportPos() does not
-                // report the new info yet, so we must predict where the
-                // view has shifted to
+                // Menu disappeared, reported view info is wrong
                 view.x -= std::min(menu_shift, view.x);
+
+                bool is_area = check_area();
+                if (is_area)
+                {
+                    width -= (Gui::AREA_MAP_WIDTH + 1);
+                }
+
+                // At the right edge of the map, it's always shifted
+                // over so the right side of the viewport is menu_shift
+                // tiles from the right edge of the map
+                view.x = std::min(view.x, static_cast<int16_t>(map.x - width - menu_shift));
             }
             last_view = view; last_cursor = cursor;
         }
-        else if (!is_default && was_default &&
-                 Gui::getViewportPos() == last_view &&
-                 last_cursor.isValid() && cur_cursor.isValid())
+        else if (!is_default && cur_cursor.isValid() && last_cursor.isValid())
         {
-            Gui::setCursorCoords(last_cursor.x, last_cursor.y, last_cursor.z);
-            Gui::refreshSidebar();
-        }
-        else if (!is_default && cur_cursor.isValid())
-        {
-            last_cursor = df::coord();
+            if (was_default && view == last_view)
+            {
+                Gui::setCursorCoords(last_cursor.x, last_cursor.y, last_cursor.z);
+                Gui::refreshSidebar();
+            }
+            else
+            {
+                last_cursor = df::coord();
+            }
         }
     }
 };

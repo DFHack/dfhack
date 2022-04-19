0f113d99ef85a8ed5730d64d53692978648e8df2
Tim Siegel <siegeltr@gmail.com>
2022-04-19 10:11:46 -0400

    [tweak] fast-trade: Fix Shift+UP to select current item
    The DOWN and UP keys should both work the same way: toggle the item on
    the *current* line, then move the cursor (down or up) to the next item.
    
    While here, since the logic is the same when moving goods to the depot
    and during the trade, it is cleaner to pull it out into a common
    definition.

--- plugins/tweak/tweaks/fast-trade.h
+++ plugins/tweak/tweaks/fast-trade.h
@@ -1,59 +1,44 @@
 using namespace std;
 
+static df::interface_key fast_trade_move_key(const set<df::interface_key> *input)
+{
+    return input->count(interface_key::CURSOR_DOWN_FAST)
+         ? interface_key::STANDARDSCROLL_DOWN
+         : input->count(interface_key::CURSOR_UP_FAST)
+         ? interface_key::STANDARDSCROLL_UP
+         : interface_key::NONE;
+}
+
+#define DEFINE_FAST_TRADE_INTERPOSE(is_trading)                             \
+    DEFINE_VMETHOD_INTERPOSE(void, feed, (set<df::interface_key> *input))   \
+    {                                                                       \
+        df::interface_key move_key;                                         \
+        if ((is_trading) && (move_key = fast_trade_move_key(input)))        \
+        {                                                                   \
+            set<df::interface_key> tmp;                                     \
+            tmp.insert(interface_key::SELECT);                              \
+            INTERPOSE_NEXT(feed)(&tmp);                                     \
+                                                                            \
+            tmp.clear();                                                    \
+            tmp.insert(move_key);                                           \
+            INTERPOSE_NEXT(feed)(&tmp);                                     \
+        }                                                                   \
+        else                                                                \
+            INTERPOSE_NEXT(feed)(input);                                    \
+    }
+
+
 struct fast_trade_assign_hook : df::viewscreen_layer_assigntradest {
     typedef df::viewscreen_layer_assigntradest interpose_base;
 
-    DEFINE_VMETHOD_INTERPOSE(void, feed, (set<df::interface_key> *input))
-    {
-        if (layer_objects[1]->active && input->count(interface_key::CURSOR_DOWN_FAST))
-        {
-            set<df::interface_key> tmp; tmp.insert(interface_key::SELECT);
-            INTERPOSE_NEXT(feed)(&tmp);
-            tmp.clear(); tmp.insert(interface_key::STANDARDSCROLL_DOWN);
-            INTERPOSE_NEXT(feed)(&tmp);
-        }
-        else if (layer_objects[1]->active && input->count(interface_key::CURSOR_UP_FAST))
-        {
-            set<df::interface_key> tmp; tmp.insert(interface_key::STANDARDSCROLL_UP);
-            INTERPOSE_NEXT(feed)(&tmp);
-            tmp.clear(); tmp.insert(interface_key::SELECT);
-            INTERPOSE_NEXT(feed)(&tmp);
-        }
-        else
-            INTERPOSE_NEXT(feed)(input);
-    }
+    DEFINE_FAST_TRADE_INTERPOSE(layer_objects[1]->active)
 };
 
 IMPLEMENT_VMETHOD_INTERPOSE(fast_trade_assign_hook, feed);
 
 struct fast_trade_select_hook : df::viewscreen_tradegoodsst {
     typedef df::viewscreen_tradegoodsst interpose_base;
-
-    DEFINE_VMETHOD_INTERPOSE(void, feed, (set<df::interface_key> *input))
-    {
-        if (!(is_unloading || !has_traders || in_edit_count)
-            && input->count(interface_key::CURSOR_DOWN_FAST))
-        {
-            set<df::interface_key> tmp; tmp.insert(interface_key::SELECT);
-            INTERPOSE_NEXT(feed)(&tmp);
-            if (in_edit_count)
-                INTERPOSE_NEXT(feed)(&tmp);
-            tmp.clear(); tmp.insert(interface_key::STANDARDSCROLL_DOWN);
-            INTERPOSE_NEXT(feed)(&tmp);
-        }
-        else if (!(is_unloading || !has_traders || in_edit_count)
-            && input->count(interface_key::CURSOR_UP_FAST))
-        {
-            set<df::interface_key> tmp; tmp.insert(interface_key::STANDARDSCROLL_UP);
-            INTERPOSE_NEXT(feed)(&tmp);
-            tmp.clear(); tmp.insert(interface_key::SELECT);
-            INTERPOSE_NEXT(feed)(&tmp);
-            if (in_edit_count)
-                INTERPOSE_NEXT(feed)(&tmp);
-        }
-        else
-            INTERPOSE_NEXT(feed)(input);
-    }
+    DEFINE_FAST_TRADE_INTERPOSE(has_traders && !is_unloading && !in_edit_count)
 };
 
 IMPLEMENT_VMETHOD_INTERPOSE(fast_trade_select_hook, feed);

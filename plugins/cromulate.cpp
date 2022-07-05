40fdfd083f330a0ff90261ad35e07c58bbb7a3a2
Myk <myk002@yahoo.com>
2022-07-05 14:13:25 -0700

    add sample plugin to go with the sample help (#2239)

---
+++ plugins/cromulate.cpp
@@ -0,0 +1,28 @@
+#include "Core.h"
+#include <Console.h>
+#include <Export.h>
+#include <PluginManager.h>
+
+using namespace DFHack;
+using namespace df::enums;
+
+DFHACK_PLUGIN("cromulate");
+
+command_result cromulate (color_ostream &out, std::vector <std::string> & parameters);
+
+DFhackCExport command_result plugin_init (color_ostream &out, std::vector <PluginCommand> &commands) {
+    commands.push_back(PluginCommand("cromulate",
+                                     "in-cpp plugin short desc", //to use one line in the ``[DFHack]# ls`` output
+                                     cromulate,
+                                     false,
+                                     "in-cpp plugin long help"));
+    return CR_OK;
+}
+
+DFhackCExport command_result plugin_shutdown (color_ostream &out) {
+    return CR_OK;
+}
+
+command_result cromulate (color_ostream &out, std::vector <std::string> &parameters) {
+    return CR_OK;
+}

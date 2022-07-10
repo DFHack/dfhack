8b3027af1b8eb628f7d8ca12a940ea26f4db35ef
myk002 <myk002@yahoo.com>
2022-07-09 23:58:11 -0700

    ensure all files are reread on every docs build
    this fixes the issue where the Stonesense docs were getting ignored

--- docs/build.sh
+++ docs/build.sh
@@ -11,5 +11,5 @@
 cd $(dirname "$0")
 cd ..
 
-"${SPHINX:-sphinx-build}" -b html -d build/docs/html . docs/html -w build/docs/html/_sphinx-warnings.txt -j "${JOBS:-auto}" "$@"
-"${SPHINX:-sphinx-build}" -b text -d build/docs/text . docs/text -w build/docs/text/_sphinx-warnings.txt -j "${JOBS:-auto}" "$@"
+"${SPHINX:-sphinx-build}" -E -b html -d build/docs/html . docs/html -w build/docs/html/_sphinx-warnings.txt -j "${JOBS:-auto}" "$@"
+"${SPHINX:-sphinx-build}" -E -b text -d build/docs/text . docs/text -w build/docs/text/_sphinx-warnings.txt -j "${JOBS:-auto}" "$@"

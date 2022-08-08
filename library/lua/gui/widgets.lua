22f9f3b042b615f22ab23b8590fdb6e6b7b76b45
myk002 <myk002@yahoo.com>
2022-08-08 11:44:33 -0700

    add unit tests for new widget functionality

--- library/lua/gui/widgets.lua
+++ library/lua/gui/widgets.lua
@@ -307,10 +307,10 @@ function EditField:onInput(keys)
     elseif keys.CURSOR_LEFT then
         self:setCursor(self.cursor - 1)
         return true
-    elseif keys.A_MOVE_W_DOWN then -- Ctrl-Left (prev word start)
-        local _, prev_word_start = self.text:sub(1, self.cursor-1):
-                                                    find('.*[^%w_%-]+[%w_%-]')
-        self:setCursor(prev_word_start or 1)
+    elseif keys.A_MOVE_W_DOWN then -- Ctrl-Left (end of prev word)
+        local _, prev_word_end = self.text:sub(1, self.cursor-1):
+                                                    find('.*[%w_%-][^%w_%-]')
+        self:setCursor(prev_word_end or 1)
         return true
     elseif keys.A_CARE_MOVE_W then -- Alt-Left (home)
         self:setCursor(1)
@@ -318,9 +318,9 @@ function EditField:onInput(keys)
     elseif keys.CURSOR_RIGHT then
         self:setCursor(self.cursor + 1)
         return true
-    elseif keys.A_MOVE_E_DOWN then -- Ctrl-Right (next word end)
-        local _, next_word_end = self.text:find('[%w_%-]+[^%w_%-]', self.cursor)
-        self:setCursor(next_word_end)
+    elseif keys.A_MOVE_E_DOWN then -- Ctrl-Right (beginning of next word)
+        local _,next_word_start = self.text:find('[^%w_%-][%w_%-]', self.cursor)
+        self:setCursor(next_word_start)
         return true
     elseif keys.A_CARE_MOVE_E then -- Alt-Right (end)
         self:setCursor()

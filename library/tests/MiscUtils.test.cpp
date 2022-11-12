25f87306b427c39e14591e19754d56633e940b92
Josh Cooper <cooper.s.josh@outlook.com>
2022-11-11 18:46:46 -0800

    Removes separated unit test executables

--- library/tests/MiscUtils.test.cpp
+++ library/tests/MiscUtils.test.cpp
@@ -1,26 +1,19 @@
 
 #include "MiscUtils.h"
 #include <gtest/gtest.h>
-#include <iostream>
 #include <string>
 
 TEST(MiscUtils, wordwrap) {
     std::vector<std::string> result;
 
-    std::cout << "MiscUtils.wordwrap: 0 wrap" << "... ";
     word_wrap(&result, "123", 3);
     ASSERT_EQ(result.size(), 1);
-    std::cout << "ok\n";
 
     result.clear();
-    std::cout << "MiscUtils.wordwrap: 1 wrap" << "... ";
     word_wrap(&result, "12345", 3);
     ASSERT_EQ(result.size(), 2);
-    std::cout << "ok\n";
 
     result.clear();
-    std::cout << "MiscUtils.wordwrap: 2 wrap" << "... ";
     word_wrap(&result, "1234567", 3);
     ASSERT_EQ(result.size(), 3);
-    std::cout << "ok\n";
 }

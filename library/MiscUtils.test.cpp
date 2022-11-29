88074dacf047f9002fdba88039f59c5e89c05c62
myk002 <myk002@yahoo.com>
2022-11-28 17:31:10 -0800

    move tests into the same dir as the main files

--- library/MiscUtils.test.cpp
+++ library/MiscUtils.test.cpp
@@ -1,47 +1,19 @@
-#include "Internal.h"
+
 #include "MiscUtils.h"
-
-#include <iostream>
+#include <gtest/gtest.h>
 #include <string>
-using namespace std;
 
-int compare_result(const vector<string> &expect, const vector<string> &result)
-{
-    if (result == expect)
-    {
-        cout << "ok\n";
-        return 0;
-    }
-    else {
-        cout << "not ok\n";
-        auto e = expect.begin();
-        auto r = result.begin();
-        cout << "# " << setw(20) << left << "Expected" << " " << left << "Got\n";
-        while (e < expect.end() || r < result.end())
-        {
-            cout
-                << "# "
-                << setw(20) << left << ":" + (e < expect.end() ? *e++ : "") + ":"
-                << " "
-                << setw(20) << left << ":" + (r < result.end() ? *r++ : "") + ":"
-                << "\n";
-        }
-        return 1;
-    }
-}
+TEST(MiscUtils, wordwrap) {
+    std::vector<std::string> result;
 
-int main()
-{
-    int fails = 0;
-#define TEST_WORDWRAP(label, expect, args) \
-    { \
-        vector<string> result; \
-        cout << label << "... "; \
-        word_wrap args; \
-        fails += compare_result(expect, result); \
-    }
+    word_wrap(&result, "123", 3);
+    ASSERT_EQ(result.size(), 1);
 
-    TEST_WORDWRAP("Short line, no wrap", vector<string>({"12345"}), (&result, "12345", 15));
+    result.clear();
+    word_wrap(&result, "12345", 3);
+    ASSERT_EQ(result.size(), 2);
 
-    return fails == 0 ? 0 : 1;
+    result.clear();
+    word_wrap(&result, "1234567", 3);
+    ASSERT_EQ(result.size(), 3);
 }

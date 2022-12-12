e923f6ccca25926be836b8de3397b88fa4de7723
Kelly Kinkade <kelly.lynn.martin@gmail.com>
2022-12-12 04:00:35 -0600

    update batch files for vs2022 compat

--- build/win64/build-debug.bat
+++ build/win64/build-debug.bat
@@ -1,4 +1,4 @@
-call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 cd VC2022
 msbuild /m /p:Platform=x64 /p:Configuration=RelWithDebInfo ALL_BUILD.vcxproj
 cd ..

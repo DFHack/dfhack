eac6dc62eeb4bb636f4306d5335d1b2a4feb37ef
Rose <japa.mala.illo@gmail.com>
2022-12-17 11:24:31 -0800

    Update windows build scripts

--- build/win64/package-debug.bat
+++ build/win64/package-debug.bat
@@ -1,6 +1,6 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-cd VC2015
+call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+cd VC2022
 msbuild /m /p:Platform=x64 /p:Configuration=RelWithDebInfo PACKAGE.vcxproj
 cd ..
 exit %ERRORLEVEL%

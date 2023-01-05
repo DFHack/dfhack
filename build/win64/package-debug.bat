acb4a3287211cde73e448c7ff2d239b787d85b80
Amber Brown <hawkowl@atleastfornow.net>
2023-01-05 13:03:15 +1100

    support VS2022 Community + build tools

--- build/win64/package-debug.bat
+++ build/win64/package-debug.bat
@@ -1,5 +1,9 @@
 @echo off
-call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" (
+    call "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64
+) else (
+    call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+)
 cd VC2022
 msbuild /m /p:Platform=x64 /p:Configuration=RelWithDebInfo PACKAGE.vcxproj
 cd ..

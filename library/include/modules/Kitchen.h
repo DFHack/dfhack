be40d55e64e2a5fd1f211e845a2a81bdbc0c6184
Myk Taylor <myk002@yahoo.com>
2023-01-09 17:31:49 -0800

    update seedwatch

--- library/include/modules/Kitchen.h
+++ library/include/modules/Kitchen.h
@@ -27,11 +27,6 @@ distribution.
 * kitchen settings
 */
 #include "Export.h"
-#include "Module.h"
-#include "Types.h"
-#include "VersionInfo.h"
-#include "Core.h"
-#include "modules/Items.h"
 #include "df/kitchen_exc_type.h"
 
 /**
@@ -43,37 +38,20 @@ namespace DFHack
 {
 namespace Kitchen
 {
+// print the exclusion list, with the material index also translated into its token (for organics) - for debug really
+DFHACK_EXPORT void debug_print(color_ostream &out);
+
 /**
  * Kitchen exclusions manipulator. Currently geared towards plants and seeds.
  * \ingroup grp_modules
  * \ingroup grp_kitchen
  */
 
-// print the exclusion list, with the material index also translated into its token (for organics) - for debug really
-DFHACK_EXPORT void debug_print(color_ostream &out);
-
 // remove this plant from the exclusion list if it is in it
 DFHACK_EXPORT void allowPlantSeedCookery(int32_t plant_id);
 
 // add this plant to the exclusion list, if it is not already in it
 DFHACK_EXPORT void denyPlantSeedCookery(int32_t plant_id);
-
-// fills a map with info from the limit info storage entries in the exclusion list
-DFHACK_EXPORT void fillWatchMap(std::map<int32_t, int16_t>& watchMap);
-
-// Finds the index of a limit info storage entry. Returns -1 if not found.
-DFHACK_EXPORT int findLimit(int32_t plant_id);
-
-// removes a limit info storage entry from the exclusion list if it's present
-DFHACK_EXPORT bool removeLimit(int32_t plant_id);
-
-// add a limit info storage item to the exclusion list, or alters an existing one
-DFHACK_EXPORT bool setLimit(int32_t plant_id, int16_t limit);
-
-// clears all limit info storage items from the exclusion list
-DFHACK_EXPORT void clearLimits();
-
-DFHACK_EXPORT std::size_t size();
 
 // Finds the index of a kitchen exclusion in plotinfo.kitchen.exc_types. Returns -1 if not found.
 DFHACK_EXPORT int findExclusion(df::kitchen_exc_type type,
@@ -89,6 +67,8 @@ DFHACK_EXPORT bool addExclusion(df::kitchen_exc_type type,
 DFHACK_EXPORT bool removeExclusion(df::kitchen_exc_type type,
     df::item_type item_type, int16_t item_subtype,
     int16_t mat_type, int32_t mat_index);
+
+DFHACK_EXPORT std::size_t size();
 
 }
 }

d14a3b2c8312ed42f74a85c6c4b4b52c9c0b9d19
Myk Taylor <myk002@yahoo.com>
2023-01-15 00:49:30 -0800

    ensure the untested warning doesn't bork on bad scripts

--- library/lua/dfhack.lua
+++ library/lua/dfhack.lua
@@ -698,11 +698,14 @@ local valid_script_flags = {
 local warned_scripts = {}
 
 function dfhack.run_script(name,...)
-    if not warned_scripts[name] and require('helpdb').get_entry_tags(name).untested then
-        warned_scripts[name] = true
-        dfhack.printerr(('UNTESTED WARNING: the "%s" script has not been validated to work well with this version of DF.'):format(name))
-        dfhack.printerr('It may not work as expected, or it may corrupt your game.')
-        qerror('Please run the command again to ignore this warning and proceed.')
+    if not warned_scripts[name] then
+        local helpdb = require('helpdb')
+        if helpdb.is_entry(name) and helpdb.get_entry_tags(name).untested then
+            warned_scripts[name] = true
+            dfhack.printerr(('UNTESTED WARNING: the "%s" script has not been validated to work well with this version of DF.'):format(name))
+            dfhack.printerr('It may not work as expected, or it may corrupt your game.')
+            qerror('Please run the command again to ignore this warning and proceed.')
+        end
     end
 
     return dfhack.run_script_with_env(nil, name, nil, ...)

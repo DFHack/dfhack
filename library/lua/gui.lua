e5072a4f64fb3d6add4c4b6e08e89571ff56fe09
Myk Taylor <myk002@yahoo.com>
2023-02-13 09:26:13 -0800

    check for settings in the ATTRS, not the args

--- library/lua/gui.lua
+++ library/lua/gui.lua
@@ -708,9 +708,9 @@ ZScreen.ATTRS{
 }
 
 function ZScreen:preinit(args)
-    if args.initial_pause == nil then
+    if self.ATTRS.initial_pause == nil then
         args.initial_pause = DEFAULT_INITIAL_PAUSE or
-                (args.pass_mouse_clicks == false)
+                (self.ATTRS.pass_mouse_clicks == false)
     end
 end
 

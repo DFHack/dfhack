c56e24803453e267b2e6cda0f984f099de370a16
Myk Taylor <myk002@yahoo.com>
2023-03-10 14:08:31 -0800

    cleanup stockpiles plugin and don't bork on empty type vectors

--- plugins/stockpiles/stockpiles.cpp
+++ plugins/stockpiles/stockpiles.cpp
@@ -1,3 +1,4 @@
+#include "Debug.h"
 #include "PluginManager.h"
 #include "StockpileUtils.h"
 #include "StockpileSerializer.h"
@@ -12,11 +13,16 @@ using namespace DFHack;
 
 DFHACK_PLUGIN("stockpiles");
 
-static command_result savestock ( color_ostream &out, vector <string> & parameters );
-static command_result loadstock ( color_ostream &out, vector <string> & parameters );
+REQUIRE_GLOBAL(world);
 
-DFhackCExport command_result plugin_init ( color_ostream &out, std::vector <PluginCommand> &commands )
-{
+namespace DFHack {
+    DBG_DECLARE(stockpiles, log, DebugCategory::LINFO);
+}
+
+static command_result savestock(color_ostream& out, vector <string>& parameters);
+static command_result loadstock(color_ostream& out, vector <string>& parameters);
+
+DFhackCExport command_result plugin_init(color_ostream& out, std::vector <PluginCommand>& commands) {
     commands.push_back(PluginCommand(
         "savestock",
         "Save the active stockpile's settings to a file.",
@@ -31,61 +37,46 @@ DFhackCExport command_result plugin_init ( color_ostream &out, std::vector <Plug
     return CR_OK;
 }
 
-DFhackCExport command_result plugin_shutdown ( color_ostream &out )
-{
+DFhackCExport command_result plugin_shutdown(color_ostream& out) {
     return CR_OK;
 }
 
 //  exporting
-static command_result savestock ( color_ostream &out, vector <string> & parameters )
-{
-    df::building_stockpilest *sp = Gui::getSelectedStockpile(out, true);
-    if ( !sp )
-    {
-        out.printerr ( "Selected building isn't a stockpile.\n" );
+static command_result savestock(color_ostream& out, vector <string>& parameters) {
+    df::building_stockpilest* sp = Gui::getSelectedStockpile(out, true);
+    if (!sp) {
+        out.printerr("Selected building isn't a stockpile.\n");
         return CR_WRONG_USAGE;
     }
 
-    if ( parameters.size() > 2 )
-    {
-        out.printerr ( "Invalid parameters\n" );
+    if (parameters.size() > 2) {
+        out.printerr("Invalid parameters\n");
         return CR_WRONG_USAGE;
     }
 
-    bool debug = false;
     std::string file;
-    for ( size_t i = 0; i < parameters.size(); ++i )
-    {
-        const std::string o = parameters.at ( i );
-        if ( o == "--debug"  ||  o ==  "-d" )
-            debug =  true;
-        else  if ( !o.empty() && o[0] !=  '-' )
-        {
+    for (size_t i = 0; i < parameters.size(); ++i) {
+        const std::string o = parameters.at(i);
+        if (!o.empty() && o[0] != '-') {
             file = o;
         }
     }
-    if ( file.empty() )
-    {
-        out.printerr ( "You must supply a valid filename.\n" );
+    if (file.empty()) {
+        out.printerr("You must supply a valid filename.\n");
         return CR_WRONG_USAGE;
     }
 
-    StockpileSerializer cereal ( sp );
-    if ( debug )
-        cereal.enable_debug ( out );
+    StockpileSerializer cereal(sp);
 
-    if ( !is_dfstockfile ( file ) ) file += ".dfstock";
-    try
-    {
-        if ( !cereal.serialize_to_file ( file ) )
-        {
-            out.printerr ( "could not save to %s\n", file.c_str() );
+    if (!is_dfstockfile(file)) file += ".dfstock";
+    try {
+        if (!cereal.serialize_to_file(file)) {
+            out.printerr("could not save to %s\n", file.c_str());
             return CR_FAILURE;
         }
     }
-    catch ( std::exception &e )
-    {
-        out.printerr ( "serialization failed: protobuf exception: %s\n", e.what() );
+    catch (std::exception& e) {
+        out.printerr("serialization failed: protobuf exception: %s\n", e.what());
         return CR_FAILURE;
     }
 
@@ -94,59 +85,45 @@ static command_result savestock ( color_ostream &out, vector <string> & paramete
 
 
 // importing
-static command_result loadstock ( color_ostream &out, vector <string> & parameters )
-{
-    df::building_stockpilest *sp = Gui::getSelectedStockpile(out, true);
-    if ( !sp )
-    {
-        out.printerr ( "Selected building isn't a stockpile.\n" );
+static command_result loadstock(color_ostream& out, vector <string>& parameters) {
+    df::building_stockpilest* sp = Gui::getSelectedStockpile(out, true);
+    if (!sp) {
+        out.printerr("Selected building isn't a stockpile.\n");
         return CR_WRONG_USAGE;
     }
 
-    if ( parameters.size() < 1 ||  parameters.size() > 2 )
-    {
-        out.printerr ( "Invalid parameters\n" );
+    if (parameters.size() < 1 || parameters.size() > 2) {
+        out.printerr("Invalid parameters\n");
         return CR_WRONG_USAGE;
     }
 
-    bool debug = false;
     std::string file;
-    for ( size_t i = 0; i < parameters.size(); ++i )
-    {
-        const std::string o = parameters.at ( i );
-        if ( o == "--debug"  ||  o ==  "-d" )
-            debug =  true;
-        else  if ( !o.empty() && o[0] !=  '-' )
-        {
+    for (size_t i = 0; i < parameters.size(); ++i) {
+        const std::string o = parameters.at(i);
+        if (!o.empty() && o[0] != '-') {
             file = o;
         }
     }
-    if ( file.empty() ) {
-        out.printerr ( "ERROR: missing .dfstock file parameter\n");
+    if (file.empty()) {
+        out.printerr("ERROR: missing .dfstock file parameter\n");
         return DFHack::CR_WRONG_USAGE;
     }
-    if ( !is_dfstockfile ( file ) )
+    if (!is_dfstockfile(file))
         file += ".dfstock";
-    if ( !Filesystem::exists ( file ) )
-    {
-        out.printerr ( "ERROR: the .dfstock file doesn't exist: %s\n",  file.c_str());
+    if (!Filesystem::exists(file)) {
+        out.printerr("ERROR: the .dfstock file doesn't exist: %s\n", file.c_str());
         return CR_WRONG_USAGE;
     }
 
-    StockpileSerializer cereal ( sp );
-    if ( debug )
-        cereal.enable_debug ( out );
-    try
-    {
-        if ( !cereal.unserialize_from_file ( file ) )
-        {
-            out.printerr ( "unserialization failed: %s\n", file.c_str() );
+    StockpileSerializer cereal(sp);
+    try {
+        if (!cereal.unserialize_from_file(file)) {
+            out.printerr("unserialization failed: %s\n", file.c_str());
             return CR_FAILURE;
         }
     }
-    catch ( std::exception &e )
-    {
-        out.printerr ( "unserialization failed: protobuf exception: %s\n", e.what() );
+    catch (std::exception& e) {
+        out.printerr("unserialization failed: protobuf exception: %s\n", e.what());
         return CR_FAILURE;
     }
     return CR_OK;

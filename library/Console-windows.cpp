bebf3584badfd753ed7f9ad5dd70dcf604ee9d94
Myk Taylor <myk002@yahoo.com>
2023-05-02 21:06:46 -0700

    maybe fix terminal in foreground issue

--- library/Console-windows.cpp
+++ library/Console-windows.cpp
@@ -508,6 +508,7 @@ bool Console::init(bool)
     inited = true;
     // DOESN'T WORK - locks up DF!
     // ForceForegroundWindow(d->MainWindow);
+    SetForegroundWindow(d->MainWindow);
     return true;
 }
 // FIXME: looks awfully empty, doesn't it?
@@ -608,6 +609,7 @@ void Console::msleep (unsigned int msec)
 bool Console::hide()
 {
     ShowWindow( GetConsoleWindow(), SW_HIDE );
+    SetForegroundWindow(d->MainWindow);
     return true;
 }
 

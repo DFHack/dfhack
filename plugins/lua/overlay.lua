6aede45135c5b14d0c8cbfefb8540fd846d75e9c
Myk Taylor <myk002@yahoo.com>
2023-05-16 12:05:53 -0700

    add links to quickstart-guide and gui/control-panel to the title screen

--- plugins/lua/overlay.lua
+++ plugins/lua/overlay.lua
@@ -580,7 +580,8 @@ TitleVersionOverlay.ATTRS{
     default_pos={x=7, y=2},
     default_enabled=true,
     viewscreens='title/Default',
-    frame={w=35, h=3},
+    frame={w=35, h=5},
+    autoarrange_subviews=1,
 }
 
 function TitleVersionOverlay:init()
@@ -601,6 +602,20 @@ function TitleVersionOverlay:init()
             frame={t=0, l=0},
             text=text,
             text_pen=COLOR_WHITE,
+        },
+        widgets.HotkeyLabel{
+            frame={l=0},
+            label='Quickstart guide',
+            auto_width=true,
+            key='STRING_A063',
+            on_activate=function() dfhack.run_script('quickstart-guide') end,
+        },
+        widgets.HotkeyLabel{
+            frame={l=0},
+            label='Control panel',
+            auto_width=true,
+            key='STRING_A047',
+            on_activate=function() dfhack.run_script('gui/control-panel') end,
         },
     }
 end

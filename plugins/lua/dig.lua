d22502e1e1f6f9a0c40d0cc6787efce8d7d59140
Myk Taylor <myk002@yahoo.com>
2023-11-03 23:24:55 -0700

    render overlay widget

--- plugins/lua/dig.lua
+++ plugins/lua/dig.lua
@@ -1,10 +1,15 @@
 local _ENV = mkmodule('plugins.dig')
 
+local gui = require('gui')
 local overlay = require('plugins.overlay')
 local pathable = require('plugins.pathable')
+local widgets = require('gui.widgets')
 
 WarmDampOverlay = defclass(WarmDampOverlay, overlay.OverlayWidget)
 WarmDampOverlay.ATTRS{
+    default_pos={x=50,y=-6},
+    default_enabled=true,
+    version=2,
     viewscreens={
         'dwarfmode/Designate/DIG_DIG',
         'dwarfmode/Designate/DIG_REMOVE_STAIRS_RAMPS',
@@ -17,11 +22,35 @@ WarmDampOverlay.ATTRS{
         'dwarfmode/Designate/DIG_TO_MARKER',
     },
     default_enabled=true,
-    overlay_only=true,
+    frame={w=40, h=3},
+    frame_style=gui.FRAME_MEDIUM,
 }
 
-function WarmDampOverlay:onRenderFrame()
+function WarmDampOverlay:init()
+    self:addviews{
+        widgets.ToggleHotkeyLabel{
+            frame={t=0, l=0},
+            key='CUSTOM_CTRL_W'
+            label='Dig through warm/damp tiles',
+            initial_option=false,
+        },
+    }
+end
+
+function WarmDampOverlay:onRenderFrame(dc, rect)
+    WarmDampOverlay.super.onRenderFrame(self, dc, rect)
     pathable.paintScreenWarmDamp()
+end
+
+local
+function WarmDampOverlay:onInput(keys)
+    if WarmDampOverlay.super.onInput(self, keys) then
+        return true
+    end
+    if keys._MOUSE_L_DOWN then
+        local pos = dfhack.gui.getMousePos()
+
+    end
 end
 
 CarveOverlay = defclass(CarveOverlay, overlay.OverlayWidget)

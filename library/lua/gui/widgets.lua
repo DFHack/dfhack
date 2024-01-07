c6164244ab2f97fc23c6835fd4b62747acc7c10a
Myk Taylor <myk002@yahoo.com>
2024-01-07 00:09:53 -0800

    implement and document new Divider widget

--- library/lua/gui/widgets.lua
+++ library/lua/gui/widgets.lua
@@ -58,6 +58,73 @@ function Widget:onRenderFrame(dc, rect)
     end
 end
 
+-------------
+-- Divider --
+-------------
+
+Divider = defclass(Divider, Widget)
+Divider.ATTRS{
+    frame_style=gui.FRAME_THIN,
+    external=false,
+    frame_style_t=DEFAULT_NIL,
+    external_t=DEFAULT_NIL,
+    frame_style_b=DEFAULT_NIL,
+    external_b=DEFAULT_NIL,
+    frame_style_l=DEFAULT_NIL,
+    external_l=DEFAULT_NIL,
+    frame_style_r=DEFAULT_NIL,
+    external_r=DEFAULT_NIL,
+}
+
+local function divider_get_val(self, base_name, edge_name)
+    local val = self[base_name..'_'..edge_name]
+    if val ~= nil then return val end
+    return self[base_name]
+end
+
+local function divider_get_junction_pen(self, edge_name)
+    local external = divider_get_val(self, 'external', edge_name)
+    local pen_name = ('%sT%s_frame_pen'):format(edge_name, external and 'e' or 'i')
+    local frame_style = divider_get_val(self, 'frame_style', edge_name)
+    if type(frame_style) == 'function' then
+        frame_style = frame_style()
+    end
+    return frame_style[pen_name]
+end
+
+function Divider:onRenderBody(dc)
+    local rect, style = self.frame_rect, self.frame_style
+    if type(style) == 'function' then
+        style = style()
+    end
+
+    if rect.height == 1 and rect.width == 1 then
+        dc:seek(0, 0):char(nil, style.x_frame_pen)
+    elseif rect.width == 1 then
+        local fill_start, fill_end = 0, rect.height-1
+        if self.frame_style_t ~= false then
+            fill_start = 1
+            dc:seek(0, 0):char(nil, divider_get_junction_pen(self, 't'))
+        end
+        if self.frame_style_b ~= false then
+            fill_end = rect.height-2
+            dc:seek(0, rect.height-1):char(nil, divider_get_junction_pen(self, 'b'))
+        end
+        dc:fill(0, fill_start, 0, fill_end, style.v_frame_pen)
+    else
+        local fill_start, fill_end = 0, rect.width-1
+        if self.frame_style_l ~= false then
+            fill_start = 1
+            dc:seek(0, 0):char(nil, divider_get_junction_pen(self, 'l'))
+        end
+        if self.frame_style_r ~= false then
+            fill_end = rect.width-2
+            dc:seek(rect.width-1, 0):char(nil, divider_get_junction_pen(self, 'r'))
+        end
+        dc:fill(fill_start, 0, fill_end, 0, style.h_frame_pen)
+    end
+end
+
 -----------
 -- Panel --
 -----------

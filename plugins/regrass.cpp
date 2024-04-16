ebbe5bfb59ba90a24814ca45fd05103b64dd6636
Ryan Williams <ryan.williams.1990@gmail.com>
2024-04-16 16:32:41 -0700

    Update regrass.cpp

--- plugins/regrass.cpp
+++ plugins/regrass.cpp
@@ -1,3 +1,4 @@
+#include "Debug.h"
 #include "modules/Gui.h"
 #include "modules/Maps.h"
 #include "PluginManager.h"
@@ -21,6 +22,11 @@ using namespace DFHack;
 DFHACK_PLUGIN("regrass");
 REQUIRE_GLOBAL(world);
 
+namespace DFHack
+{
+    DBG_DECLARE(regrass, log, DebugCategory::LINFO);
+}
+
 command_result df_regrass(color_ostream &out, vector<string> &parameters);
 
 struct regrass_options
@@ -35,9 +41,10 @@ struct regrass_options
 
 #define P_R_FLAG(btype) case biome_type::btype: return plant_raw_flags::BIOME_##btype
 
-static df::plant_raw_flags biome_flag(df::biome_type biome_type) // TODO: attr? update gui/biomes; plant_index mapping?
+static df::plant_raw_flags biome_flag(df::biome_type biome) // TODO: attr?
 {
-    switch (biome_type)
+    DEBUG(log).print("Getting plant raw flag for biome_type::%s\n", ENUM_KEY_STR(biome_type, biome).c_str());
+    switch (biome)
     {
         P_R_FLAG(MOUNTAIN);
         P_R_FLAG(GLACIER);
@@ -92,14 +99,15 @@ static df::plant_raw_flags biome_flag(df::biome_type biome_type) // TODO: attr? 
         P_R_FLAG(SUBTERRANEAN_LAVA);
     }
 
-    Core::printerr("Bad biome %d!\n", biome_type);
-    return plant_raw_flags::TREE; // Always false
+    Core::printerr("Bad biome %d!\n", biome);
+    return plant_raw_flags::BIOME_SUBTERRANEAN_LAVA;
 }
 
 #undef P_R_FLAG
 
-static bool valid_tile(df::map_block *block, int x, int y, regrass_options options) // Is valid tile for regrass
-{
+static bool valid_tile(df::map_block *block, int x, int y, regrass_options options)
+{   // Is valid tile for regrass
+    auto des = block->designation[x][y];
     auto tt = block->tiletype[x][y];
     auto shape = tileShape(tt);
     auto mat = tileMaterial(tt);
@@ -108,13 +116,14 @@ static bool valid_tile(df::map_block *block, int x, int y, regrass_options optio
     if (mat == tiletype_material::GRASS_LIGHT ||
         mat == tiletype_material::GRASS_DARK ||
         mat == tiletype_material::PLANT) // Shrubs and saplings can have grass underneath
-    {
-        return true; // Refill existing grass
+    {   // Refill existing grass
+        DEBUG(log).print("Valid tile: Grass|Shrub|Sapling\n");
+        return true;
     }
     else if (tt == tiletype::TreeTrunkPillar)
     {   // Trees can have grass under main tile
         auto p = df::coord(block->map_pos.x + x, block->map_pos.y + y, block->map_pos.z);
-        auto mbc = world->map.column_index[(block->map_pos.x / 48) * 3][(block->map_pos.y / 48) * 3];
+        auto mbc = world->map.column_index[(block->map_pos.x / 48)*3][(block->map_pos.y / 48)*3];
         if (!mbc)
         {
             Core::printerr("No MLT block column for tile at (%d, %d, %d)!\n", p.x, p.y, p.z);
@@ -124,38 +133,67 @@ static bool valid_tile(df::map_block *block, int x, int y, regrass_options optio
         for (auto plant : mbc->plants)
         {
             if (plant->pos == p)
+            {
+                DEBUG(log).print("Valid tile: Tree\n");
                 return true; // Is main tile
+            }
         }
 
+        DEBUG(log).print("Invalid tile: Tree\n");
         return false; // Not main tile
     }
-    else if (block->designation[x][y].bits.flow_size > 2)
-        return false; // Under water/magma; TODO: research
+    else if (des.bits.flow_size > (des.bits.liquid_type == tile_liquid::Magma ? 0 : 3))
+    {   // Under water/magma (df::plant_raw::shrub_drown_level is usually 4)
+        DEBUG(log).print("Invalid tile: Liquid\n");
+        return false;
+    }
     else if (shape != tiletype_shape::FLOOR &&
         shape != tiletype_shape::RAMP &&
         shape != tiletype_shape::STAIR_UP &&
         shape != tiletype_shape::STAIR_DOWN &&
         shape != tiletype_shape::STAIR_UPDOWN)
     {
+        DEBUG(log).print("Invalid tile: Shape\n");
         return false;
     }
-    else if (block->occupancy[x][y].bits.building != tile_building_occ::None || // TODO: research (planned, stockpiles)
-        block->occupancy[x][y].bits.no_grow) // TODO: pebbles?
+    else if (block->occupancy[x][y].bits.building != tile_building_occ::None)
     {
+        DEBUG(log).print("Invalid tile: Building\n");
+        return false;
+    }
+    else if (block->occupancy[x][y].bits.no_grow)
+    {
+        DEBUG(log).print("Invalid tile: no_grow\n");
         return false;
     }
     else if (mat == tiletype_material::SOIL)
-        return spec != tiletype_special::FURROWED && spec != tiletype_special::WET; // Beach; TODO: research (beach biome? driftwood?)
-    else if (options.ashes && mat == tiletype_material::ASHES)
+    {
+        if (spec == tiletype_special::FURROWED || spec == tiletype_special::WET)
+        {   // Dirt road or beach
+            DEBUG(log).print("Invalid tile: Furrowed|Wet\n");
+            return false;
+        }
+
+        DEBUG(log).print("Valid tile: Soil\n");
         return true;
+    }
+    else if (options.ashes && mat == tiletype_material::ASHES)
+    {
+        DEBUG(log).print("Valid tile: Ashes\n");
+        return true;
+    }
     else if (options.mud)
     {
         if (spec == tiletype_special::SMOOTH || spec == tiletype_special::TRACK)
-            return false; // Don't replace smoothed stone
+        {   // Don't replace smoothed stone
+            DEBUG(log).print("Invalid tile: Smooth (mud check)\n");
+            return false;
+        }
         else if (mat != tiletype_material::STONE &&
             mat != tiletype_material::LAVA_STONE &&
-            mat != tiletype_material::MINERAL) // TODO: will DF replace FEATURE?
-        {
+            mat != tiletype_material::MINERAL)
+        {   // Not non-feature stone
+            DEBUG(log).print("Invalid tile: Wrong mat (mud check)\n");
             return false;
         }
 
@@ -166,63 +204,84 @@ static bool valid_tile(df::map_block *block, int x, int y, regrass_options optio
 
             auto &ms_ev = *(df::block_square_event_material_spatterst *)blev;
             if (ms_ev.mat_type == builtin_mats::MUD)
-                return ms_ev.amount[x][y] > 0;
+            {
+                if (ms_ev.amount[x][y] > 0)
+                {
+                    DEBUG(log).print("Valid tile: Muddy stone\n");
+                    return true;
+                }
+                else
+                {
+                    DEBUG(log).print("Invalid tile: Non-muddy stone\n");
+                    return false;
+                }
+            }
         }
     }
 
+    DEBUG(log).print("Invalid tile: No success\n");
     return false;
 }
 
-static vector<int32_t> grasses_for_tile(df::map_block *block, int x, int y) // Return sorted vector of valid grass ids
-{
+static vector<int32_t> grasses_for_tile(df::map_block *block, int x, int y)
+{   // Return sorted vector of valid grass ids
     vector<int32_t> grasses;
 
+    DEBUG(log).print("Collecting grasses...\n");
     if (block->designation[x][y].bits.subterranean)
     {
-        for (auto p_raw : world->raws.plants.grasses) // Sorted by df::plant_raw::index
-        {
+        for (auto p_raw : world->raws.plants.grasses)
+        {   // Sorted by df::plant_raw::index
             if (p_raw->flags.is_set(plant_raw_flags::BIOME_SUBTERRANEAN_WATER))
+            {
+                DEBUG(log).print("Cave grass: %s\n", p_raw->id.c_str());
                 grasses.push_back(p_raw->index);
+            }
         }
     }
     else // Above ground
     {
-        auto rgn_pos = Maps::getBlockTileBiomeRgn(block, df::coord2d(x, y)); // Okay to use x,y here
+        auto rgn_pos = Maps::getBlockTileBiomeRgn(block, df::coord2d(x, y)); // x&15 is okay
 
-        if (!rgn_pos.isValid()) // No biome (happens in sky)
+        if (!rgn_pos.isValid())
+        {   // No biome (happens in sky)
+            DEBUG(log).print("No grass: No biome region!\n");
             return grasses;
+        }
 
         auto &biome_info = *Maps::getRegionBiome(rgn_pos);
         auto plant_biome_flag = biome_flag(Maps::getBiomeType(rgn_pos.x, rgn_pos.y));
 
         bool good = (biome_info.evilness < 33);
-        bool evil = (biome_info.evilness > 65); // TODO: does necro modify separately?
+        bool evil = (biome_info.evilness > 65);
         bool savage = (biome_info.savagery > 65);
 
-        for (auto p_raw : world->raws.plants.grasses) // Sorted by df::plant_raw::index
-        {
+        for (auto p_raw : world->raws.plants.grasses)
+        {   // Sorted by df::plant_raw::index
             auto &flags = p_raw->flags;
             if (flags.is_set(plant_biome_flag) &&
                 (good || !flags.is_set(plant_raw_flags::GOOD)) &&
                 (evil || !flags.is_set(plant_raw_flags::EVIL)) &&
                 (savage || !flags.is_set(plant_raw_flags::SAVAGE)))
             {
+                DEBUG(log).print("Surface grass: %s\n", p_raw->id.c_str());
                 grasses.push_back(p_raw->index);
             }
         }
     }
 
+    DEBUG(log).print("Done collecting grasses.\n");
     return grasses;
 }
 
-static bool regrass_events(df::map_block *block, int x, int y, regrass_options options) // Modify grass block events
-{
+static bool regrass_events(df::map_block *block, int x, int y, regrass_options options)
+{   // Modify grass block events
     if (!valid_tile(block, x, y, options))
         return false;
 
     bool success = false;
-    for (auto blev : block->block_events) // Refill existing events
-    {
+    for (auto blev : block->block_events)
+    {   // Try to refill existing events
         if (blev->getType() != block_square_event_type::grass)
             continue;
 
@@ -236,22 +295,25 @@ static bool regrass_events(df::map_block *block, int x, int y, regrass_options o
         else if (gr_ev.amount[x][y] > 0)
         {   // Refill first non-zero grass
             gr_ev.amount[x][y] = 100;
+            DEBUG(log).print("Refilled existing grass.\n");
             return true;
         }
     }
 
     auto valid_grasses = grasses_for_tile(block, x, y);
-    if (options.new_grass && !valid_grasses.empty()) // Try to add new grass events
+    if (options.new_grass && !valid_grasses.empty())
     {
+        DEBUG(log).print("Adding missing grasses...\n");
         auto new_grasses(valid_grasses); // Copy vector
-        for (auto blev : block->block_events) // Find which grasses we're missing
-        {
+        for (auto blev : block->block_events)
+        {   // Find which grasses we're missing
             if (blev->getType() == block_square_event_type::grass)
                 erase_from_vector(new_grasses, ((df::block_square_event_grassst *)blev)->plant_index);
         }
 
-        for (auto id : new_grasses) // Add new grass events
-        {
+        for (auto id : new_grasses)
+        {   // Add new grass events
+            DEBUG(log).print("Adding grass with plant index %d\n", id);
             auto gr_ev = df::allocate<df::block_square_event_grassst>();
             block->block_events.push_back(gr_ev);
             gr_ev->plant_index = id;
@@ -262,14 +324,18 @@ static bool regrass_events(df::map_block *block, int x, int y, regrass_options o
                 success = true;
             }
         }
+        DEBUG(log).print("Done adding grasses.\n");
     }
 
-    if (options.max_grass) // Already as full as it can get
+    if (options.max_grass)
+    {
+        DEBUG(log).print("Tile grasses maxed.\n");
         return success;
+    }
 
     vector<df::block_square_event_grassst *> temp;
-    for (auto blev : block->block_events) // Gather all valid grass events
-    {
+    for (auto blev : block->block_events)
+    {   // Gather all valid grass events
         if (blev->getType() != block_square_event_type::grass)
             continue;
 
@@ -282,14 +348,17 @@ static bool regrass_events(df::map_block *block, int x, int y, regrass_options o
     if (gr_ev)
     {
         gr_ev->amount[x][y] = 100;
+        DEBUG(log).print("Random regrass plant index %d\n", gr_ev->plant_index);
         return true;
     }
 
-    return false; // Can't support grass
+    DEBUG(log).print("Tile doesn't support grass! new_grass = %s\n", options.new_grass ? "true" : "false");
+    return false;
 }
 
 int regrass_tile(df::map_block *block, int x, int y, regrass_options options)
-{
+{   // Regrass single tile. Return 1 if tile success, else 0
+    DEBUG(log).print("Regrass tile (%d, %d, %d)\n", block->map_pos.x + x, block->map_pos.y + y, block->map_pos.z);
     if (!regrass_events(block, x, y, options))
         return 0;
 
@@ -301,15 +370,16 @@ int regrass_tile(df::map_block *block, int x, int y, regrass_options options)
         mat == tiletype_material::GRASS_DARK ||
         mat == tiletype_material::PLANT ||
         tt == tiletype::TreeTrunkPillar)
-    {
-        return 1; // Already appropriate tile
+    {   // Already appropriate tile
+        DEBUG(log).print("Tiletype no change.\n");
+        return 1;
     }
     else if (mat == tiletype_material::STONE ||
         mat == tiletype_material::LAVA_STONE ||
         mat == tiletype_material::MINERAL)
-    {   // Muddy stone
-        for (auto blev : block->block_events) // Remove mud spatter
-        {
+    {   // Muddy non-feature stone
+        for (auto blev : block->block_events)
+        {   // Remove mud spatter
             if (blev->getType() != block_square_event_type::material_spatter)
                 continue;
 
@@ -317,17 +387,22 @@ int regrass_tile(df::map_block *block, int x, int y, regrass_options options)
             if (ms_ev.mat_type == builtin_mats::MUD)
             {
                 ms_ev.amount[x][y] = 0;
+                DEBUG(log).print("Removed tile mud.\n");
                 break;
             }
         }
     }
 
-    if (shape == tiletype_shape::FLOOR) // Handle random variant, ashes
+    if (shape == tiletype_shape::FLOOR)
+    {   // Handle random variant, ashes
+        DEBUG(log).print("Tiletype to random grass floor.\n");
         block->tiletype[x][y] = findRandomVariant((rand() & 1) ? tiletype::GrassLightFloor1 : tiletype::GrassDarkFloor1);
+    }
     else
     {
         auto new_mat = (rand() & 1) ? tiletype_material::GRASS_LIGHT : tiletype_material::GRASS_DARK;
         auto new_tt = findTileType(shape, new_mat, tiletype_variant::NONE, tiletype_special::NONE, nullptr);
+        DEBUG(log).print("Tiletype to %s.\n", ENUM_KEY_STR(tiletype, new_tt).c_str());
         block->tiletype[x][y] = new_tt;
     }
 
@@ -335,7 +410,7 @@ int regrass_tile(df::map_block *block, int x, int y, regrass_options options)
 }
 
 int regrass_block(df::map_block *block, regrass_options options)
-{
+{   // Regrass single block
     int count = 0;
     for (int y = 0; y < 16; y++)
     {
@@ -347,7 +422,7 @@ int regrass_block(df::map_block *block, regrass_options options)
 }
 
 int regrass_map(regrass_options options)
-{
+{   // Regrass entire map
     int count = 0;
     for (auto block : world->map.map_blocks)
         count += regrass_block(block, options);
@@ -368,21 +443,22 @@ command_result df_regrass(color_ostream &out, vector<string> &parameters)
 {
     regrass_options options;
 
-    for (size_t i = 0; i < parameters.size(); i++) // TODO: "help"? better method
+    for (size_t i = 0; i < parameters.size(); i++)
     {
-        if (parameters[i] == "max")
+        auto &s = parameters[i];
+        if (s == "m" || s == "max")
             options.max_grass = true;
-        else if (parameters[i] == "new")
+        else if (s == "n" || s == "new")
             options.new_grass = true;
-        else if (parameters[i] == "ashes")
+        else if (s == "a" || s == "ashes")
             options.ashes = true;
-        else if (parameters[i] == "mud")
+        else if (s == "u" || s == "mud")
             options.mud = true;
-        else if (parameters[i] == "here")
+        else if (s == "p" || s == "here")
             options.here = true;
-        else if (parameters[i] == "block")
+        else if (s == "b" || s == "block")
             options.block = true;
-        else
+        else // "h", "help", invalid
             return CR_WRONG_USAGE;
     }
 
@@ -412,12 +488,21 @@ command_result df_regrass(color_ostream &out, vector<string> &parameters)
         }
 
         if (options.block)
+        {
+            DEBUG(log).print("Regrassing block...\n");
             count = regrass_block(b, options);
+        }
         else
+        {
+            DEBUG(log).print("Regrassing single tile...\n");
             count = regrass_tile(b, p.x&15, p.y&15, options);
+        }
     }
     else
+    {
+        DEBUG(log).print("Regrassing map...\n");
         count = regrass_map(options);
+    }
 
     out.print("Regrew %d tiles of grass.\n", count);
 

813abe1e44476114020b0cc83b20a88038ed3339
Ryan Williams <ryan.williams.1990@gmail.com>
2024-04-26 11:09:19 -0700

    Update LuaApi.cpp

--- library/LuaApi.cpp
+++ library/LuaApi.cpp
@@ -2462,8 +2462,18 @@ static int maps_getTileBiomeRgn(lua_State *L)
 
 static int maps_getPlantAtTile(lua_State *L)
 {
-    auto pos = CheckCoordXYZ(L, 1, true);
-    Lua::PushDFObject(L, Maps::getPlantAtTile(pos));
+    if (lua_isboolean(L, 2))
+    {
+        df::coord pos;
+        Lua::CheckDFAssign(L, &pos, 1);
+        Lua::PushDFObject(L, Maps::getPlantAtTile(pos, lua_toboolean(L, 2)));
+    }
+    else
+    {
+        auto pos = CheckCoordXYZ(L, 1, true);
+        Lua::PushDFObject(L, Maps::getPlantAtTile(pos, lua_toboolean(L, 4)));
+    }
+    
     return 1;
 }
 

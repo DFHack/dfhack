80d0f6131f50c446a2fe1e577118dde618643661
Christian Doczkal <20443222+chdoc@users.noreply.github.com>
2024-06-23 18:39:20 +0200

    new API functions for the Job module

--- library/modules/Job.cpp
+++ library/modules/Job.cpp
@@ -35,6 +35,7 @@ distribution.
 #include "modules/Job.h"
 #include "modules/Materials.h"
 #include "modules/Items.h"
+#include "modules/References.h"
 
 #include "df/building.h"
 #include "df/general_ref.h"
@@ -270,6 +271,17 @@ void DFHack::Job::printJobDetails(color_ostream &out, df::job *job)
         printItemDetails(out, job->job_items.elements[i], i);
 }
 
+bool Job::addGeneralRef(df::job *job, df::general_ref_type type, int32_t id)
+{
+    auto ref = References::createGeneralRef(type);
+    if (!ref)
+        return false;
+    ref->setID(id);
+    job->general_refs.push_back(ref);
+    return true;
+}
+
+
 df::general_ref *Job::getGeneralRef(df::job *job, df::general_ref_type type)
 {
     CHECK_NULL_POINTER(job);
@@ -407,6 +419,24 @@ bool DFHack::Job::removeJob(df::job* job) {
     volatile auto cancel_job_method = &df::job_handler::cancel_job;
     (world->jobs.*cancel_job_method)(job);
 
+    return true;
+}
+
+bool DFHack::Job::addWorker(df::job *job, df::unit *unit){
+    CHECK_NULL_POINTER(job);
+    CHECK_NULL_POINTER(unit);
+
+    if (unit->job.current_job)
+        return false;
+
+    if (job->posting_index != -1){
+        removePostings(job);
+    }
+
+    if (!addGeneralRef(job, general_ref_type::UNIT_WORKER, unit->id))
+        return false;
+    unit->job.current_job = job;
+    job->recheck_cntdn = 0;
     return true;
 }
 

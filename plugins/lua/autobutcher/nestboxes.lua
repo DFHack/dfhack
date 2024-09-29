3a86a04af45570c9f57f06c6a8760a97c9e0b636
Ryszard Pantoł <birkow@gmail.com>
2024-09-29 12:30:42 +0200

    add info about race to messages

--- plugins/lua/autobutcher/nestboxes.lua
+++ plugins/lua/autobutcher/nestboxes.lua
@@ -295,13 +295,13 @@ function checkItemCreated(item_id)
 
   local autobutcher_info = getInfoFromAutobutcher(item.race)
   if not ((autobutcher_info.watched and autobutcher_info.enabled) or state.ignore_autobutcher) then
-    printDetails('race not watched by autobutcher, autobutcher settings respected')
+    printDetails(('race %s not watched by autobutcher, autobutcher settings respected'):format(item.race))
     return
   end
 
   local race_config = getConfigForRace(item.race)
   if race_config == nil then
-    printDetails('new race without config and no new races are being watched')
+    printDetails(('race %s without config and no new races are being watched'):format(item.race))
     return
   end
 
@@ -311,7 +311,7 @@ function checkItemCreated(item_id)
   then
     nestboxesEvent.handleEggs(item, race_config.target, race_config.ama, state.split_stacks, autobutcher_info.mac)
   elseif (race_config.stop and autobutcher_info.mac == 0 and not state.ignore_autobutcher) then
-    printDetails(('Did not check eggs, missing animal count for %s is 0 and stop once animal target reached is enabled'):format(item.race))
+    printDetails(('Did not check eggs, missing animal count for race %s is 0 and stop once animal target reached is enabled'):format(item.race))
   elseif ( not race_config.watched) then
     printDetails(('Race %s not watched by %s'):format(item.race, GLOBAL_KEY))
   end

18ef51b7c09bee3426dddf80f040181237ea2a6b
Myk Taylor <myk002@yahoo.com>
2024-10-04 20:24:48 -0700

    fix status output; update changelog

--- plugins/lua/timestream.lua
+++ plugins/lua/timestream.lua
@@ -24,35 +24,9 @@ local function do_reset()
 end
 
 local function print_status()
-    print(GLOBAL_KEY .. ' is ' .. (state.enabled and 'enabled' or 'not enabled'))
+    print('timestream is ' .. (isEnabled() and 'enabled' or 'not enabled'))
     print()
-    print('settings:')
-    for _,v in ipairs(SETTINGS) do
-        print(('  %15s: %s'):format(v.name, state.settings[v.internal_name or v.name]))
-    end
-    if DEBUG < 2 then return end
-    print()
-    print(('cur_year_tick:    %d'):format(df.global.cur_year_tick))
-    print(('timeskip_deficit: %.2f'):format(timeskip_deficit))
-    if DEBUG < 3 then return end
-    print()
-    print('tick coverage:')
-    for coverage_slot=0,49 do
-        print(('  slot %2d: %scovered'):format(coverage_slot, tick_coverage[coverage_slot] and '' or 'NOT '))
-    end
-    print()
-    local bdays, bdays_list = {}, {}
-    for _, next_bday in pairs(birthday_triggers) do
-        if not bdays[next_bday] then
-            bdays[next_bday] = true
-            table.insert(bdays_list, next_bday)
-        end
-    end
-    print(('%d birthdays:'):format(#bdays_list))
-    table.sort(bdays_list)
-    for _,bday in ipairs(bdays_list) do
-        print(('  year tick: %d'):format(bday))
-    end
+    print('target FPS is set to: ' .. tostring(timestream_getFps()))
 end
 
 function parse_commandline(args)

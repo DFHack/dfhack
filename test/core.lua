a645fbc07894cb8d53a7ac00ea4c28c8fcc732e1
Myk Taylor <myk002@yahoo.com>
2024-11-28 15:41:49 -0800

    add regression test for size of global_table

--- test/core.lua
+++ test/core.lua
@@ -38,3 +38,23 @@ function test.getHackPath_chdir()
         expect.eq(clean_path(dfhack.getHackPath()), clean_path(fs.getcwd()))
     end)
 end
+
+-- validates that the size in df.globals.xml is correct
+function test.global_table_size()
+    local elem_size = df.global_table_entry:sizeof()
+    local declared_arr_size = df.global.global_table:sizeof() // elem_size
+    for i=0,declared_arr_size-1 do
+        expect.true_(df._displace(df.global.global_table[0], i, elem_size).address,
+            'nil address found in middle of global_table at idx ' .. i)
+    end
+    if df._displace(df.global.global_table[0], declared_arr_size, elem_size).address then
+        local idx_of_nil
+        for i=declared_arr_size+1,declared_arr_size*2 do
+            if not df._displace(df.global.global_table[0], i, elem_size).address then
+                idx_of_nil = i
+                break
+            end
+        end
+        expect.fail('nil *not* found at end of declared global-table (idx ' .. declared_arr_size .. '); table size should be: ' .. idx_of_nil)
+    end
+end

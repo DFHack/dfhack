4e4bea3f222808468cbcf8530632e81e0fdb0551
Myk <myk.taylor@gmail.com>
2024-12-04 06:50:48 -0800

    add clarifying comment for test

--- test/modules/job_fortress.lua
+++ test/modules/job_fortress.lua
@@ -8,6 +8,8 @@ function test.jobIDsAreSorted()
     local is_sorted = true
     local prev_id = nil
 
+    -- assumes there are at least some "naturally added" jobs currently in the list
+    -- but this should always be true for CI test saves
     for _, job in utils.listpairs(df.global.world.jobs.list) do
         if prev_id and job.id < prev_id then
             is_sorted = false

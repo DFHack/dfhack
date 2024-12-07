464ef96a1763abc32b075e8981a552c54f40acfd
dhthwy <302825+dhthwy@users.noreply.github.com>
2024-12-07 14:56:41 -0500

    Revert "#5031: Remove debug prints"
    This reverts commit ec83a702ffe8c545882b18c8cecc5efdeac8bc87.

--- plugins/pathable.cpp
+++ plugins/pathable.cpp
@@ -369,6 +369,38 @@ static void check_wagon_tile(FloodCtx & ctx, const df::coord & pos) {
         return;
 
     auto shape = tileShape(*tt);
+
+#if 0
+    if (shape == df::tiletype_shape::RAMP_TOP) {
+        std::cerr << "check_wagon_tile start" << std::endl;
+        bool r = is_wagon_traversible(ctx, pos+df::coord(-1, -1, 0), pos);
+        std::cerr << r << ", " + coord_to_str(pos+df::coord(-1, -1, 0)) + ", pos=" + coord_to_str(pos) << std::endl;
+
+	r = is_wagon_traversible(ctx, pos+df::coord( 0, -1, 0), pos);
+        std::cerr << r << ", " + coord_to_str(pos+df::coord(0, -1, 0)) + ", pos=" + coord_to_str(pos) << std::endl;
+
+	r = is_wagon_traversible(ctx, pos+df::coord( 1, -1, 0), pos);
+        std::cerr << r << ", " + coord_to_str(pos+df::coord(1, -1, 0)) + ", pos=" + coord_to_str(pos) << std::endl;
+
+	r = is_wagon_traversible(ctx, pos+df::coord(-1,  0, 0), pos);
+        std::cerr << r << ", " + coord_to_str(pos+df::coord(-1, 0, 0)) + ", pos=" + coord_to_str(pos) << std::endl;
+
+        r = is_wagon_traversible(ctx, pos+df::coord( 1,  0, 0), pos);
+	std::cerr << r << ", " + coord_to_str(pos+df::coord(1, 0, 0)) + ", pos=" + coord_to_str(pos) << std::endl;
+
+	r = is_wagon_traversible(ctx, pos+df::coord( -1,  1, 0), pos);
+        std::cerr << r << ", " + coord_to_str(pos+df::coord(-1, 1, 0)) + ", pos=" + coord_to_str(pos) << std::endl;
+
+	r = is_wagon_traversible(ctx, pos+df::coord( 0,  1, 0), pos);
+        std::cerr << r << ", " + coord_to_str(pos+df::coord(0, 1, 0)) + ", pos=" + coord_to_str(pos) << std::endl;
+
+	r = is_wagon_traversible(ctx, pos+df::coord( 1,  1, 0), pos);
+        std::cerr << r << ", " + coord_to_str(pos+df::coord(1, 1, 0)) + ", pos=" + coord_to_str(pos) << std::endl;
+        std::cerr << "check_wagon_tile end" << std::endl;
+    }
+#endif
+
+
     if ((shape != df::tiletype_shape::RAMP_TOP &&
             (is_wagon_traversible(ctx, pos+df::coord(-1, -1, 0), pos) &&
             is_wagon_traversible(ctx, pos+df::coord( 0, -1, 0), pos) &&

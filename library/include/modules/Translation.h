5389ec05ada690e3fb34ecf2799f8cd8701f576e
Quietust <quietust@gmail.com>
2025-01-04 22:05:33 -0600

    add Translation::GenerateName() (fixes #5166)
    Also available from Lua as "dfhack.generateName(...)"

--- library/include/modules/Translation.h
+++ library/include/modules/Translation.h
@@ -38,6 +38,7 @@ distribution.
 namespace df {
     struct language_name;
     struct language_translation;
+    struct language_word_table;
 }
 
 namespace DFHack
@@ -58,6 +59,8 @@ DFHACK_EXPORT std::string capitalize(const std::string &str, bool all_words = fa
 // translate a name using the loaded dictionaries
 DFHACK_EXPORT std::string TranslateName (const df::language_name * name, bool inEnglish = false,
                                          bool onlyLastPart = false);
+
+DFHACK_EXPORT void GenerateName(df::language_name *name, int language_index, df::language_name_type nametype, df::language_word_table *major_selector, df::language_word_table *minor_selector);
 }
 }
 #endif

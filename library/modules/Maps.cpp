ce425f730fba40d83737223cc1bc26a1e030e099
Dan Van Twisk <vantwisk@MacBookPro.lan>
2025-05-18 00:51:48 -0400

    Safe getTileBlock

--- library/modules/Maps.cpp
+++ library/modules/Maps.cpp
@@ -363,6 +363,16 @@ df::map_block *Maps::getTileBlock (int32_t x, int32_t y, int32_t z)
 {
     if (!isValidTilePos(x,y,z))
         return NULL;
+
+    auto &block_index = world->map.block_index;
+
+    if (block_index == NULL ||
+        block_index[bx] == NULL ||
+        block_index[bx][by] == NULL ||
+        block_index[bx][by][z] == NULL) {
+        return NULL;
+    }
+
     return world->map.block_index[x >> 4][y >> 4][z];
 }
 

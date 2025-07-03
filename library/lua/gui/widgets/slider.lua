6707e20d9062483aa314c798fc21251dce9d2d81
Squid Coder <squid@squidcoder.com>
2025-07-03 11:43:30 -0500

    clean up the sliders and how they work.
    i have split out as much of the logic as possible. couldnt split out the `get_width_per_odx` bc we need the `frame_body` to not be nil.

--- library/lua/gui/widgets/slider.lua
+++ library/lua/gui/widgets/slider.lua
@@ -1,22 +1,60 @@
--- slider.lua
-local RangeSlider = require('gui.widgets.range_slider')
+local core = require('gui.widgets.slide_core')
 
-local Slider = defclass(Slider, RangeSlider)
-Slider.ATTRS {
-    get_idx_fn = DEFAULT_NIL, -- Function to get the current index
-    on_change = DEFAULT_NIL, -- Function to handle index change
-    is_single = true
+--------------------------------
+-- Slider
+--------------------------------
+
+---@class widgets.Slider.attrs: widgets.Widget.attrs
+---@field get_idx_fn? function
+---@field on_change? fun(index: integer)
+---@field is_single boolean
+
+
+---@class widgets.Slider.attrs.partial: widgets.Slider.attrs
+
+---@class widgets.Slider.initTable: widgets.Slider.attrs
+---@field num_stops integer
+
+---@class widgets.Slider: widgets.Widget, widgets.Slider.attrs
+---@field super widgets.Widget
+---@field ATTRS widgets.Slider.attrs|fun(attributes: widgets.Slider.attrs.partial)
+---@overload fun(init_table: widgets.Slider.initTable): self
+Slider = defclass(Slider, slide_core)
+Slider.ATTRS{
+    get_idx_fn=DEFAULT_NIL,
+    on_change=DEFAULT_NIL,
+    num_stops=DEFAULT_NIL,
+    is_single=true,
 }
 
-function Slider:init()
-    self.get_left_idx_fn = self.get_idx_fn
-    self.get_right_idx_fn = self.get_idx_fn
-    self.on_left_change = function(index)
-        if self.on_change then self.on_change(index) end
+function Slider:get_width_per_idx()
+    local min_value = (self.is_single) and 3 or 5  -- Single slider = 3, else 5
+    return math.max(min_value, (self.frame_body.width-7) // (self.num_stops-1))
+end
+
+function Slider:onInput(keys)
+    if not keys._MOUSE_L then return false end
+    local x = self:getMousePos()
+    if not x then return false end
+    local left_idx, right_idx = self.get_idx_fn(), self.get_idx_fn()
+    local width_per_idx = self:get_width_per_idx()
+    local left_pos = width_per_idx*(left_idx-1)
+    local right_pos = width_per_idx*(right_idx-1) + 4
+    if x < left_pos then
+        self.on_change(self.get_idx_fn() - 1)
+    elseif x < left_pos+3 then
+        self.is_dragging_target = 'left'
+        self.is_dragging_idx = x - left_pos
+    elseif x < right_pos then
+        self.is_dragging_target = 'both'
+        self.is_dragging_idx = x - left_pos
+    elseif x < right_pos+3 then
+        self.is_dragging_target = 'right'
+        self.is_dragging_idx = x - right_pos
+    else
+        self.on_change(self.get_idx_fn() + 1)
     end
-    self.on_right_change = function(index)
-        if self.on_change then self.on_change(index) end
-    end
+    return true
 end
 
 return Slider

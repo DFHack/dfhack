b91188208eb7a00f87253da60b26242f6589bde0
Kelly Kinkade <kelly.lynn.martin@gmail.com>
2025-11-15 20:49:08 -0600

    take three?

--- library/include/df/custom/hash/labor_kitchen_interface_food_key.h
+++ library/include/df/custom/hash/labor_kitchen_interface_food_key.h
@@ -1,16 +1,26 @@
-template<>
-struct std::hash<> {
-    auto operator()(const df::labor_kitchen_interface_food_key &a) const -> size_t {
-        struct thing {
-            int16_t t;
-            int16_t st;
-            int32_t x;
-        } thing{
-            .t = a.type;
-            .st = a.subtype,
-            .x = static_cast<int32_t>(a.mat) ^ (static_cast<int32_t>(a.matg))
-        };
+#include <functional>
+#include <bit>
+#include "df/labor_kitchen_interface_food_key.h"
 
-        return hash<int64_t>()(bit_cast<int64_t>(thing));
+namespace std
+{
+    template<>
+    struct hash<df::labor_kitchen_interface_food_key>
+    {
+        auto operator()(const df::labor_kitchen_interface_food_key& a) const -> size_t
+        {
+            struct thing
+            {
+                int16_t t;
+                int16_t st;
+                int32_t x;
+            } thing{
+                .t = a.type,
+                .st = a.subtype,
+                .x = static_cast<int32_t>(a.mat) ^ (static_cast<int32_t>(a.matg))
+            };
+
+            return hash<int64_t>()(std::bit_cast<int64_t>(thing));
         }
     };
+}
